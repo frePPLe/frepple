5d4d5f1eb8662d4879dd406b71dbbbd3273ece77
Johan De Taeye <jdetaeye@frepple.com>
2007-06-21 08:50:10 +0000

    - Django:    - fixed bug in initial installation of demo dataset

--- contrib/django/freppledb/input/sql/plan.sql
+++ contrib/django/freppledb/input/sql/plan.sql
@@ -1,4 +1,4 @@
 
 insert into input_plan
   (id,name,description,current,lastmodified)
-  values(1,'Demo','Frepple demo model',current_date,current_timestamp);
+  values(1,'frePPLe','frePPLe demo model',current_date,current_timestamp);

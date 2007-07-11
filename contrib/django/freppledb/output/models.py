c2c62f4bf9bf3204ce42e1e24aa5b8662b9575ff
Johan De Taeye <jdetaeye@frepple.com>
2007-07-11 12:06:22 +0000

    - Django:    - Cleaning up of output model definitions

--- contrib/django/freppledb/output/models.py
+++ contrib/django/freppledb/output/models.py
@@ -28,6 +28,7 @@ from freppledb.input.models import Operation, Demand, Buffer, Resource
 LIST_PER_PAGE = 100
 
 class OperationPlan(models.Model):
+    # Database fields
     identifier = models.IntegerField(primary_key=True)
     demand = models.ForeignKey(Demand, related_name='delivery', null=True, db_index=True, raw_id_admin=True)
     operation = models.ForeignKey(Operation, related_name='instances', null=True, db_index=True, raw_id_admin=True)
@@ -38,13 +39,15 @@ class OperationPlan(models.Model):
     enddate = models.DateField(db_index=True)
     locked = models.BooleanField(default=True, radio_admin=True)
     owner = models.ForeignKey('self', null=True, blank=True, related_name='children', raw_id_admin=True)
-    def __str__(self):
-        return str(self.identifier)
+
+    def __str__(self): return str(self.identifier)
+
     class Admin:
         search_fields = ['operation']
         list_display = ('identifier', 'operation', 'startdate', 'enddate', 'quantity', 'locked', 'owner')
         list_per_page = LIST_PER_PAGE
         date_hierarchy = 'startdate'
+
     class Meta:
         permissions = (("view_operationplan", "Can view operation plans"),)
         # Ordering is buggy :-(
@@ -52,7 +55,9 @@ class OperationPlan(models.Model):
         # Ordering requires 'operation_id' and fails when it is set to 'operation'
         #ordering = ['operation_id','startdatetime']
 
+
 class Problem(models.Model):
+    # Database fields
     entity = models.CharField(maxlength=10, db_index=True)
     name = models.CharField(maxlength=20, db_index=True)
     description = models.CharField(maxlength=80)
@@ -60,19 +65,23 @@ class Problem(models.Model):
     enddatetime = models.DateTimeField('end date')
     startdate = models.DateField(db_index=True)
     enddate = models.DateField(db_index=True)
-    def __str__(self):
-        return str(self.name)
+
+    def __str__(self): return str(self.name)
+
     class Admin:
         list_display = ('entity', 'name', 'description', 'startdate', 'enddate')
         search_fields = ['description']
         date_hierarchy = 'startdate'
         list_filter = ['entity','name','startdate']
         list_per_page = LIST_PER_PAGE
+
     class Meta:
         permissions = (("view_problem", "Can view problems"),)
         ordering = ['startdatetime']
 
+
 class LoadPlan(models.Model):
+    # Database fields
     resource = models.ForeignKey(Resource, related_name='loadplans', db_index=True, raw_id_admin=True)
     operation = models.ForeignKey(Operation, related_name='loadplans', db_index=True, raw_id_admin=True)
     operationplan = models.ForeignKey(OperationPlan, related_name='loadplans', raw_id_admin=True)
@@ -81,11 +90,14 @@ class LoadPlan(models.Model):
     date = models.DateField(db_index=True)
     onhand = models.DecimalField(max_digits=10, decimal_places=2)
     maximum = models.DecimalField(max_digits=10, decimal_places=2)
+
     def __str__(self):
         return self.resource.name + ' ' + str(self.date)
+
     class Admin:
         list_display = ('resource', 'operation', 'quantity', 'date', 'onhand', 'maximum', 'operationplan')
         list_per_page = LIST_PER_PAGE
+
     class Meta:
         permissions = (("view_loadplans", "Can view load plans"),)
         # Ordering is buggy :-(
@@ -93,7 +105,9 @@ class LoadPlan(models.Model):
         # Ordering requires 'resource_id' and fails when it is set to 'resource'
         #ordering = ['resource_id','datetime']
 
+
 class FlowPlan(models.Model):
+    # Database fields
     thebuffer = models.ForeignKey(Buffer, related_name='flowplans', db_index=True, raw_id_admin=True)
     operation = models.ForeignKey(Operation, related_name='flowplans', db_index=True, raw_id_admin=True)
     operationplan = models.ForeignKey(OperationPlan, related_name='flowplans', raw_id_admin=True)
@@ -101,11 +115,14 @@ class FlowPlan(models.Model):
     datetime = models.DateTimeField()
     date = models.DateField(db_index=True)
     onhand = models.DecimalField(max_digits=10, decimal_places=2)
+
     def __str__(self):
         return self.thebuffer.name + str(self.date)
+
     class Admin:
         list_display = ('thebuffer', 'operation', 'quantity', 'date', 'onhand', 'operationplan')
         list_per_page = LIST_PER_PAGE
+
     class Meta:
         permissions = (("view_flowplans", "Can view flow plans"),)
         # Ordering is buggy :-(

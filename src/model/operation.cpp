0ab07723488480512a5daf45af5853bc25f99256
Johan De Taeye <jdetaeye@frepple.com>
2007-07-25 06:01:46 +0000

    - removed leftover debugging print

--- src/model/operation.cpp
+++ src/model/operation.cpp
@@ -255,7 +255,6 @@ DECLARE_EXPORT void OperationFixedTime::endElement (XMLInput& pIn, XMLElement& p
 DECLARE_EXPORT void OperationTimePer::setOperationPlanParameters
 (OperationPlan* oplan, float q, Date s, Date e, bool preferEnd) const
 {
-  logger << q << "  " <<s << "  " <<e << "  " <<preferEnd << endl;
   // Invalid call to the function.
   if (!oplan || q<0) return;
 

292dd14d2fd286a097f56f1aaa4cce8769d2ff8f
Johan De Taeye <jdetaeye@frepple.com>
2009-05-30 07:58:04 +0000

    - improved REST web service

--- bin/init.py
+++ bin/init.py
@@ -29,6 +29,7 @@ except:
   def RESTwebservice(address=None, port=8080):
     raise ImportError, "no module named cherrypy"    
 else:  
+  import os
   
   def RESTwebservice(address=None, port=None):
     import socket
@@ -61,11 +62,9 @@ else:
       })    
     config = {'/': 
         {
-            #'tools.trailing_slash.on': False,
             #'server.environment': 'development',
             'server.threadPool': 10,
             'engine.autoreload_on': False,
-            #'log.access_log.propagate': False ,
         }
     }
     print 'Web service starting on http://%s:%s/' % (address, port)
@@ -73,245 +72,262 @@ else:
     print 'Web service stopped'
 
   class RESTinterface:
+  
     top = [ 
       '<?xml version="1.0" encoding="UTF-8" ?>\n',
       '<plan xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\n',
       ]
+      
     bottom = [ 
       '</plan>\n',
       ]
- 
-    # Top-level interface handling URLs of the format:
-    #    PUT /
-    #    GET /
+    
+    current_dir = os.getcwd()
+    
     @cherrypy.expose
-    def POST(self):
-      return "OK"
-      
+    def testupload(self):
+        return """
+        <html><body>
+            <form action=".." method="post" enctype="multipart/form-data">
+            filename: <input type="file" name="myFile"/><br/>
+            <input type="submit"/>
+            </form>
+        </body></html>
+        """
+        
+    # Serve the XSD definition
+    @cherrypy.expose
+    def frepple_xsd(self):
+      return cherrypy.lib.static.serve_file(
+        os.path.join(self.current_dir, 'frepple.xsd'),
+        content_type='application/xml'
+        )
+        
+    # Serve the XSD definition
+    @cherrypy.expose
+    def frepple_core_xsd(self):
+      return cherrypy.lib.static.serve_file(
+        os.path.join(self.current_dir, 'frepple_core.xsd'),
+        content_type='application/xml'
+        )
+        
     # Top-level interface handling URLs of the format:
+    #    POST /
     #    PUT /
     #    GET /
     @cherrypy.expose
     def index(self):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<locations>\n')
-      for f in frepple.locations(): res.append(f.toXML())        
-      res.append('</locations>\n')
-      res.append('<customers>\n')
-      for f in frepple.customers(): res.append(f.toXML())        
-      res.append('</customers>\n')
-      res.append('<operations>\n')
-      for f in frepple.operations(): res.append(f.toXML())        
-      res.append('</operations>\n')
-      res.append('<buffers>\n')
-      for f in frepple.buffers(): res.append(f.toXML())        
-      res.append('</buffers>\n')
-      res.append('<items>\n')
-      for f in frepple.items(): res.append(f.toXML())        
-      res.append('</items>\n')
-      res.append('<flows>\n')
-      for b in frepple.buffers():      
-        for f in b.flows: res.append(f.toXML())        
-      res.append('</flows>\n')
-      res.append('<loads>\n')
-      for b in frepple.resources():      
-        for f in b.loads: res.append(f.toXML())        
-      res.append('</loads>\n')
-      res.append('<problems>\n')
-      for f in frepple.problems(): res.append(f.toXML())        
-      res.append('</problems>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+      request = cherrypy.request
+      cherrypy.response.headers['server'] = 'frepple/%s' % frepple.version
+      cherrypy.response.headers['content-type'] = 'application/xml'
+      if request.method == 'GET': 
+        # GET request 
+        res = []
+        for i in self.top: res.append(i)
+        res.append('<locations>\n')
+        for f in frepple.locations(): res.append(f.toXML())        
+        res.append('</locations>\n')
+        res.append('<customers>\n')
+        for f in frepple.customers(): res.append(f.toXML())        
+        res.append('</customers>\n')
+        res.append('<operations>\n')
+        for f in frepple.operations(): res.append(f.toXML())        
+        res.append('</operations>\n')
+        res.append('<buffers>\n')
+        for f in frepple.buffers(): res.append(f.toXML())        
+        res.append('</buffers>\n')
+        res.append('<items>\n')
+        for f in frepple.items(): res.append(f.toXML())        
+        res.append('</items>\n')
+        res.append('<flows>\n')
+        for b in frepple.buffers():      
+          for f in b.flows: res.append(f.toXML())        
+        res.append('</flows>\n')
+        res.append('<loads>\n')
+        for b in frepple.resources():      
+          for f in b.loads: res.append(f.toXML())        
+        res.append('</loads>\n')
+        res.append('<problems>\n')
+        for f in frepple.problems(): res.append(f.toXML())        
+        res.append('</problems>\n')
+        for i in self.bottom: res.append(i)
+        return "".join(res)
+      else:
+        # POST and PUT requests
+        error = []
+        for f in cherrypy.request.params:
+          try:
+            frepple.readXMLdata(cherrypy.request.params[f].file.read())       
+          except Exception, e:
+            error.append("%s : %s" % (f,e))
+        if len(error) > 0:          
+          return '\n'.join(error)
+        return "OK"
         
+    # A generic way to expose XML data.
+    # Use this decorator function to decorate a generator function. 
+    def simpleXMLdata(gen):
+      @cherrypy.expose
+      def decorator(self, *__args,**__kw):
+        cherrypy.response.headers['server'] = 'frepple/%s' % frepple.version
+        if cherrypy.request.method != 'GET': 
+          raise cherrypy.HTTPError(404,"Not found")
+        cherrypy.response.headers['content-type'] = 'application/xml'
+        res = []
+        for i in self.top: res.append(i)
+        for i in gen(__args,__kw): res.append(i)
+        for i in self.bottom: res.append(i)
+        return "".join(res)
+      return decorator
+      
     # Interface for locations handling URLs of the format:
     #    GET /location/
     #    GET /location/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def location(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<locations>\n')
+      yield '<locations>\n'
       if name:
         # Return a single location
         try:
-          res.append(frepple.location(name=name,action="C").toXML())
+          yield frepple.location(name=name,action="C").toXML()
         except:
           # Location not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all locations
-        for f in frepple.locations(): res.append(f.toXML())        
-      res.append('</locations>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.locations(): yield f.toXML()        
+      yield '</locations>\n'    
         
     # Interface for buffers handling URLs of the format:
     #    GET /buffer/
     #    GET /buffer/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def buffer(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<buffers>\n')
+      yield '<buffers>\n'
       if name:
         # Return a single buffer
         try:
-          res.append(frepple.buffer(name=name,action="C").toXML())
+          yield frepple.buffer(name=name,action="C").toXML()
         except:
           # Buffer not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all buffers
-        for f in frepple.locations(): res.append(f.toXML())        
-      res.append('</buffers>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.locations(): yield f.toXML()
+      yield '</buffers>\n'
         
     # Interface for items handling URLs of the format:
     #    GET /item/
     #    GET /item/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def item(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<items>\n')
+      yield '<items>\n'
       if name:
         # Return a single item
         try:
-          res.append(frepple.item(name=name,action="C").toXML())
+          yield frepple.item(name=name,action="C").toXML()
         except:
           # Item not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all items
-        for f in frepple.items(): res.append(f.toXML())        
-      res.append('</items>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.items(): yield f.toXML()
+      yield '</items>\n'
         
     # Interface for demands handling URLs of the format:
     #    GET /demand/
     #    GET /demand/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def demand(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<demands>\n')
+      yield '<demands>\n'
       if name:
         # Return a single demand
         try:
-          res.append(frepple.demand(name=name,action="C").toXML())
+          yield frepple.demand(name=name,action="C").toXML()
         except:
           # Demand not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all locations
-        for f in frepple.demands(): res.append(f.toXML())        
-      res.append('</demands>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.demands(): yield f.toXML()
+      yield '</demands>\n'
         
     # Interface for resources handling URLs of the format:
     #    GET /resource/
     #    GET /resource/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def resource(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<resources>\n')
+      yield '<resources>\n'
       if name:
         # Return a single resource
         try:
-          res.append(frepple.resource(name=name,action="C").toXML())
+          yield frepple.resource(name=name,action="C").toXML()
         except:
           # Resource not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all resources
-        for f in frepple.resources(): res.append(f.toXML())        
-      res.append('</resources>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.resources(): yield f.toXML()
+      yield '</resources>\n'
         
     # Interface for calendars handling URLs of the format:
     #    GET /calendar/
     #    GET /calendar/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def calendar(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<calendars>\n')
+      yield '<calendars>\n'
       if name:
         # Return a single calendar
         try:
-          res.append(frepple.calendar(name=name,action="C").toXML())
+          yield frepple.calendar(name=name,action="C").toXML()
         except:
           # Calendar not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all calendars
-        for f in frepple.calendars(): res.append(f.toXML())        
-      res.append('</calendars>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.calendars(): yield f.toXML()
+      yield '</calendars>\n'
         
     # Interface for customers handling URLs of the format:
     #    GET /customer/
     #    GET /customer/<name>/
-    @cherrypy.expose
+    @simpleXMLdata
     def customer(self, name=None):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<customers>\n')
+      yield '<customers>\n'
       if name:
         # Return a single customer
         try:
-          res.append(frepple.customer(name=name,action="C").toXML())
+          yield frepple.customer(name=name,action="C").toXML()
         except:
           # Customer not found
-          raise cherrypy.HTTPError(404,"entity not found")
+          raise cherrypy.HTTPError(404,"Entity not found")
       else:
         # Return all customers
-        for f in frepple.customers(): res.append(f.toXML())        
-      res.append('</customers>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in frepple.customers(): yield f.toXML()
+      yield '</customers>\n'
         
     # Interface for flows handling URLs of the format:
     #    GET /flow/
-    @cherrypy.expose
+    @simpleXMLdata
     def flow(self):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<flows>\n')
+      yield '<flows>\n'
       for b in frepple.buffers():      
-        for f in b.flows: res.append(f.toXML())        
-      res.append('</flows>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in b.flows: yield f.toXML()
+      yield '</flows>\n'
         
     # Interface for loads handling URLs of the format:
     #    GET /load/
-    @cherrypy.expose
+    @simpleXMLdata
     def load(self):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<loads>\n')
+      yield '<loads>\n'
       for b in frepple.resources():      
-        for f in b.loads: res.append(f.toXML())        
-      res.append('</loads>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+        for f in b.loads: yield f.toXML()
+      yield '</loads>\n'
         
     # Interface for problems handling URLs of the format:
     #    GET /problem/
-    @cherrypy.expose
+    @simpleXMLdata
     def problem(self):
-      res = []
-      for i in self.top: res.append(i)
-      res.append('<problems>\n')
-      for f in frepple.problems(): res.append(f.toXML())        
-      res.append('</problems>\n')
-      for i in self.bottom: res.append(i)
-      return "".join(res)
+      yield '<problems>\n'
+      for f in frepple.problems(): yield f.toXML()
+      yield '</problems>\n'

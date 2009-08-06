0558fac785d31b9096d14c4cf89bef4a68b59d2f
Johan De Taeye <jdetaeye@frepple.com>
2009-08-06 06:14:40 +0000

    - added missing and corrected typos in copyright headers

--- src/solver/solverdemand.cpp
+++ src/solver/solverdemand.cpp
@@ -9,7 +9,7 @@
  * Copyright (C) 2007 by Johan De Taeye                                    *
  *                                                                         *
  * This library is free software; you can redistribute it and/or modify it *
- * under the terms of the GNUt Lesser General Public License as published   *
+ * under the terms of the GNU Lesser General Public License as published   *
  * by the Free Software Foundation; either version 2.1 of the License, or  *
  * (at your option) any later version.                                     *
  *                                                                         *

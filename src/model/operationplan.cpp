07a2924ca3f55146e5e761202d643a4719e6568c
Johan De Taeye <jdetaeye@frepple.com>
2010-08-04 07:59:14 +0000

    - Correction in the excess detection loop.  Thanks Andreas at OptimalCore for spotting this.

--- src/model/operationplan.cpp
+++ src/model/operationplan.cpp
@@ -755,8 +755,8 @@ DECLARE_EXPORT bool OperationPlan::isExcess(bool strict) const
     for (; j != i->getBuffer()->getFlowPlans().end(); --j)
     {
       if ( (current_maximum > 0  
-             && j->getOnhand() < i->getQuantity() + current_maximum + ROUNDING_ERROR)
-        || j->getOnhand() < i->getQuantity() + current_minimum + ROUNDING_ERROR )
+             && j->getOnhand() < i->getQuantity() + current_maximum - ROUNDING_ERROR)
+        || j->getOnhand() < i->getQuantity() + current_minimum - ROUNDING_ERROR )
         return false;
       if (j->getType() == 4 && !strict) current_maximum = j->getMax(false);
       if (j->getType() == 3 && !strict) current_minimum = j->getMin(false);

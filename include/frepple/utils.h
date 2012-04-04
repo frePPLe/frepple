2d64f89d5fdd632e92e3d905d717e912aca5517a
Johan De Taeye <jdetaeye@frepple.com>
2012-04-04 13:58:25 +0000

    Updating release number to 0.9.2.beta

--- include/frepple/utils.h
+++ include/frepple/utils.h
@@ -127,7 +127,7 @@ using namespace std;
 #include <config.h>
 #else
 // Define the version for (windows) compilers that don't use autoconf
-#define PACKAGE_VERSION "0.9.1"
+#define PACKAGE_VERSION "0.9.2.beta"
 #endif
 
 // Header for multithreading

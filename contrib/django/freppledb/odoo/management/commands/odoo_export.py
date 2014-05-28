4ca226a8056117bcbe1a709cbeb1c4d3fc0fed53
Johan De Taeye <jdetaeye@frepple.com>
2014-05-28 16:00:23 +0200

    odoo connector: refactoring to make customizations easier

--- contrib/django/freppledb/odoo/management/commands/odoo_export.py
+++ contrib/django/freppledb/odoo/management/commands/odoo_export.py
@@ -14,18 +14,14 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from __future__ import print_function
 from optparse import make_option
-import xmlrpclib
 from datetime import datetime
-from time import time
 
 from django.core.management.base import BaseCommand, CommandError
-from django.db import transaction, connections, DEFAULT_DB_ALIAS
+from django.utils.importlib import import_module
+from django.db import transaction, DEFAULT_DB_ALIAS
 from django.conf import settings
-from django.utils import six
 
-from freppledb.common.models import Parameter
 from freppledb.execute.models import Task
 
 
@@ -44,14 +40,6 @@ class Command(BaseCommand):
   requires_model_validation = False
 
 
-  def odoo_search(self, a, b=[]):
-    return self.sock.execute(self.odoo_db, self.uid, self.odoo_password, a, 'search', b)
-
-
-  def odoo_data(self, a, b, c):
-    return self.sock.execute(self.odoo_db, self.uid, self.odoo_password, a, 'read', b, c)
-
-
   def handle(self, **options):
 
     # Pick up the options
@@ -63,20 +51,6 @@ class Command(BaseCommand):
     else: self.database = DEFAULT_DB_ALIAS
     if not self.database in settings.DATABASES.keys():
       raise CommandError("No database settings known for '%s'" % self.database )
-
-    # Pick up configuration parameters
-    self.odoo_user = Parameter.getValue("odoo.user", self.database)
-    self.odoo_password = Parameter.getValue("odoo.password", self.database)
-    self.odoo_db = Parameter.getValue("odoo.db", self.database)
-    self.odoo_url = Parameter.getValue("odoo.url", self.database)
-    if not self.odoo_user:
-      raise CommandError("Missing or invalid parameter odoo_user")
-    if not self.odoo_password:
-      raise CommandError("Missing or invalid parameter odoo_password")
-    if not self.odoo_db:
-      raise CommandError("Missing or invalid parameter odoo_db")
-    if not self.odoo_url:
-      raise CommandError("Missing or invalid parameter odoo_url")
 
     # Make sure the debug flag is not set!
     # When it is set, the django database wrapper collects a list of all SQL
@@ -94,34 +68,30 @@ class Command(BaseCommand):
       if 'task' in options and options['task']:
         try: task = Task.objects.all().using(self.database).get(pk=options['task'])
         except: raise CommandError("Task identifier not found")
-        if task.started or task.finished or task.status != "Waiting" or task.name != 'odoo export':
+        if task.started or task.finished or task.status != "Waiting" or task.name != 'Odoo export':
           raise CommandError("Invalid task identifier")
         task.status = '0%'
         task.started = now
       else:
-        task = Task(name='odoo edport', submitted=now, started=now, status='0%', user=user)
+        task = Task(name='Odoo export', submitted=now, started=now, status='0%', user=user)
       task.save(using=self.database)
       transaction.commit(using=self.database)
 
-      # Log in to the odoo server
-      sock_common = xmlrpclib.ServerProxy(self.odoo_url + 'xmlrpc/common')
-      self.uid = sock_common.login(self.odoo_db, self.odoo_user, self.odoo_password)
+      # Find the connector class
+      # We look for a module called "odoo_export" in each of the installed
+      # applications, and expect to find a class called connector in it
+      connector = None
+      for app in reversed(settings.INSTALLED_APPS):
+        try:
+          connector = getattr(import_module('%s.odoo_export' % app),'Connector')
+        except ImportError as e:
+          # Silently ignore if it's the module which isn't found in the app
+          if str(e) != 'No module named odoo_export': raise e
+      if not connector:
+          raise CommandError("No odoo_export connector found")
 
-      # Connect to odoo server
-      self.sock = xmlrpclib.ServerProxy(self.odoo_url + 'xmlrpc/object')
-
-      # Create a database connection to the frePPLe database
-      self.cursor = connections[self.database].cursor()
-
-      # Upload all data
-      self.export_procurement_order()
-      task.status = '50%'
-      task.save(using=self.database)
-      transaction.commit(using=self.database)
-      self.export_sales_order()
-      task.status = '100%'
-      task.save(using=self.database)
-      transaction.commit(using=self.database)
+      # Instantiate the connector and upload all data
+      connector(task, self.database, self.verbosity).run()
 
       # Log success
       task.status = 'Done'
@@ -140,142 +110,3 @@ class Command(BaseCommand):
       except: pass
       settings.DEBUG = tmp_debug
       transaction.set_autocommit(ac, using=self.database)
-
-
-  # Update the committed date of sales orders
-  #   - uploading for each frePPLe demand where subcategory = 'odoo'
-  #   - mapped fields frePPLe -> odoo sale.order
-  #        - max(out_demand.plandate) -> requested_date
-  # Note: Ideally we'ld like to update the committed date instead. (But it is read-only)
-  def export_sales_order(self):
-    transaction.enter_transaction_management(using=self.database)
-    try:
-      starttime = time()
-      if self.verbosity > 0:
-        print("Exporting requested date of sales orders...")
-      self.cursor.execute('''select source, max(plandate)
-          from demand
-          left outer join out_demand
-            on demand.name = out_demand.demand
-          where demand.subcategory = 'odoo'
-          group by source
-         ''')
-      cnt = 0
-      for i, j in self.cursor.fetchall():
-        if isinstance(j, six.string_types):
-          # SQLite exports the date as a string
-          j = datetime.strptime(j, "%Y-%m-%d %H:%M:%S")
-        self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'sale.order', 'write',
-          [int(i)], {'requested_date': j and j.strftime('%Y-%m-%d') or '1971-01-01',})
-        cnt += 1
-      if self.verbosity > 0:
-        print("Updated %d sales orders in %.2f seconds" % (cnt, (time() - starttime)))
-    except Exception as e:
-      raise CommandError("Error updating sales orders: %s" % e)
-    finally:
-      transaction.rollback(using=self.database)
-      transaction.leave_transaction_management(using=self.database)
-
-
-  # Upload procurement order data. They are translated in odoo into purchase orders
-  # or manufacturing orders.
-  #   - uploading frePPLe operationplans
-  #   - meeting the criterion:
-  #        - operation.subcategory = 'odoo'
-  #        - not a delivery operationplan (since procurement order is directly
-  #          created by the odoo sales order)
-  #   - mapped fields frePPLe -> odoo production order
-  #        - %id %name -> name
-  #        - operationplan.quantity -> product_qty
-  #        - operationplan.startdate -> date_planned
-  #        - operation.location_id -> location_id
-  #        - 1 (id for PCE) -> product_uom
-  #        - 1 (hardcoded...) -> company_id
-  #        - 'make_to_order' -> procure_method
-  #        - 'frePPLe' -> origin
-  #   - Note that purchase order are uploaded as quotations. Once the quotation
-  #     is created in odoo it is not revised any more by frePPLe.
-  #     For instance: if the need for the purchase disappears later on, the request still
-  #     remains in odoo. We could delete/refresh the quotations suggested by frePPLe,
-  #     but this could confuse the buyer who has already contacted the supplier or any
-  #     other work on the quotation.
-  #     FrePPLe would show an excess alert, but it's up to the planner to act on it
-  #     and cancel the purchase orders or quotations.
-  def export_procurement_order(self):
-    transaction.enter_transaction_management(using=self.database)
-    try:
-      starttime = time()
-      if self.verbosity > 0:
-        print("Canceling draft procurement orders")
-      ids = self.odoo_search('procurement.order',
-        ['|',('state','=', 'draft'),('state','=','cancel'),('origin','=', 'frePPLe'),])
-      self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'unlink', ids)
-      if self.verbosity > 0:
-        print("Cancelled %d draft procurement orders in %.2f seconds" % (len(ids), (time() - starttime)))
-      starttime = time()
-      if self.verbosity > 0:
-        print("Exporting procurement orders...")
-      self.cursor.execute('''select
-           out_operationplan.id, operation, out_operationplan.quantity,
-           enddate, location.source as location,
-           item.source as item, buffer.type
-         from out_operationplan
-         inner join out_flowplan
-           on operationplan_id = out_operationplan.id
-           and out_flowplan.quantity > 0
-         inner join buffer
-           on buffer.name = out_flowplan.thebuffer
-           and buffer.subcategory = 'odoo'
-         inner join location
-           on location.name = buffer.location_id
-           and location.subcategory = 'odoo'
-         inner join item
-           on item.name = buffer.item_id
-           and item.subcategory = 'odoo'
-         where out_operationplan.locked = 'f'
-         ''')
-      cnt = 0
-      ids_buy = []
-      ids_produce = []
-      for i, j, k, l, m, n, o in self.cursor.fetchall():
-        proc_order = {
-          'name': "%s %s" % (i,j),
-          'product_qty': str(k),
-          'date_planned': l.strftime('%Y-%m-%d'),
-          'product_id': n,
-          'company_id': 1,
-          'product_uom': 1,
-          'location_id': m,
-          'procure_method': 'make_to_order',
-          'origin': 'frePPLe'
-          }
-        proc_order_id = self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'create', proc_order)
-        if o == 'procure':
-          ids_buy.append(proc_order_id)
-        else:
-          ids_produce.append(proc_order_id)
-        cnt += 1
-      if self.verbosity > 0:
-        print("Uploaded %d procurement orders in %.2f seconds" % (cnt, (time() - starttime)))
-      starttime = time()
-      if self.verbosity > 0:
-        print("Confirming %d procurement orders into purchasing quotations" % (len(ids_buy)))
-      for i in ids_buy:
-        self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'action_confirm', [i])
-        self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'action_po_assign', [i])
-      if self.verbosity > 0:
-        print("Confirmed %d procurement orders in %.2f seconds" % (len(ids_buy), (time() - starttime)))
-      starttime = time()
-      if self.verbosity > 0:
-        print("Confirming %d procurement orders into production orders" % (len(ids_produce)))
-      for i in ids_produce:
-        self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'action_produce_assign_product', [i])
-        self.sock.execute(self.odoo_db, self.uid, self.odoo_password, 'procurement.order', 'action_confirm', [i])
-      if self.verbosity > 0:
-        print("Confirmed %d procurement orders in %.2f seconds" % (len(ids_produce), (time() - starttime)))
-    except Exception as e:
-      raise CommandError("Error exporting procurement orders: %s" % e)
-    finally:
-      transaction.rollback(using=self.database)
-      transaction.leave_transaction_management(using=self.database)
-

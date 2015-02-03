c577dd99e340dd3854b0c72c70d5cb885a9b55a9
Johan De Taeye <jdetaeye@frepple.com>
2015-02-03 20:40:58 +0100

    Error in validation of durations. Merci Remy Cherel de le rapporter!

--- contrib/django/freppledb/common/fields.py
+++ contrib/django/freppledb/common/fields.py
@@ -85,7 +85,7 @@ class DurationFormField(fields.RegexField):
 
     # Parse the input string to a decimal number, representing the number of seconds
     try:
-      t = value.split(":")
+      t = value.strip().split(":")
       tl = len(t)
       if tl <= 1:
         # Seconds only

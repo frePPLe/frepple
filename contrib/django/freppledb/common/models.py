d82d3c3becfc5d806a852a40df11ea80a05b2f7a
Johan De Taeye <jdetaeye@frepple.com>
2015-04-16 07:47:13 +0200

    indexing buckets by name

--- contrib/django/freppledb/common/models.py
+++ contrib/django/freppledb/common/models.py
@@ -277,7 +277,7 @@ class BucketDetail(AuditModel):
   # Database fields
   id = models.AutoField(_('identifier'), primary_key=True)
   bucket = models.ForeignKey(Bucket, verbose_name=_('bucket'), db_index=True)
-  name = models.CharField(_('name'), max_length=settings.NAMESIZE)
+  name = models.CharField(_('name'), max_length=settings.NAMESIZE, db_index=True)
   startdate = models.DateTimeField(_('start date'))
   enddate = models.DateTimeField(_('end date'))
 

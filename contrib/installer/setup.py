6689843d1d9b5d73501b6bc8e30dc3b656e3ab3f
Johan De Taeye <jdetaeye@frepple.com>
2015-10-27 09:54:42 +0100

    avoid packaging windows-version specific files in the py2exe installer

--- contrib/installer/setup.py
+++ contrib/installer/setup.py
@@ -126,8 +126,22 @@ setup(
           "excludes": excludes,
           "includes": includes,
           "ignores": ignores,
-          # ignore this file that is useful only in archaic windows versions
-          "dll_excludes": ['w9xpopen.exe'],
+          "dll_excludes": [
+            # Ignore this file that is useful only in archaic windows versions
+            'w9xpopen.exe',
+            # Ignore these files which are windows-specific, and exist by default
+            # in the Windows system folders if they are required. The files are not
+            # present on windows 10 anyway.
+            'api-ms-win-core-processthreads-l1-1-2.dll',
+            'api-ms-win-core-sysinfo-l1-2-1.dll',
+            'api-ms-win-core-heap-l2-1-0.dll',
+            'api-ms-win-core-delayload-l1-1-1.dll',
+            'api-ms-win-core-errorhandling-l1-1-1.dll',
+            'api-ms-win-core-profile-l1-1-0.dll',
+            'api-ms-win-core-libraryloader-l1-2-0.dll',
+            'api-ms-win-core-string-obsolete-l1-1-0.dll',
+            'api-ms-win-security-activedirectoryclient-l1-1-0.dll'
+            ],
           }},
     data_files = data_files,
     # Attributes

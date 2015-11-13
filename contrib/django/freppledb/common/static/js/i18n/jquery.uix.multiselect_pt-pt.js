c30b7301c75ca5fafdf17df1c4f63fe13303ec96
Johan De Taeye <jdetaeye@frepple.com>
2015-11-13 12:11:50 +0100

    rename some translations files

---
+++ contrib/django/freppledb/common/static/js/i18n/jquery.uix.multiselect_pt-pt.js
@@ -0,0 +1,50 @@
+/*
+ * jQuery UI Multiselect 2.0
+ *
+ * Authors:
+ *  Yanick Rochon (yanick.rochon[at]gmail[dot]com)
+ *  Translation: Alexander Bulei
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * http://mind2soft.com/labs/jquery/multiselect/
+ *
+ *
+ * Localization : PT
+ *
+ */
+
+(function($) {
+
+$.uix.multiselect.i18n['pt'] = {
+    itemsSelected_nil: 'Nenhuma opção selecionada',       // 0
+    itemsSelected: '{count} opção seleccionada',          // 0, 1
+    itemsSelected_plural: '{count} opções seleccionadas', // n
+    //itemsSelected_plural_two: ...                       // 2
+    //itemsSelected_plural_few: ...                       // 3, 4
+    itemsAvailable_nil: 'Nenhuma opção disponível',
+    itemsAvailable: '{count} opção disponível',
+    itemsAvailable_plural: '{count} opções disponíveis',
+    //itemsAvailable_plural_two: ...
+    //itemsAvailable_plural_few: ...
+    itemsFiltered_nil: 'Nenhuma opção filtrada',
+    itemsFiltered: '{count} opção filtrada',
+    itemsFiltered_plural: '{count} opções filtradas',
+    //itemsFiltered_plural_two: ...
+    //itemsFiltered_plural_few: ...
+    selectAll: 'Seleccionar todos',
+    deselectAll: 'Desseleccionar todos',
+    search: 'Opções de pesquisa',
+    collapseGroup: 'Ocultar grupo',
+    expandGroup: 'Expandir grupo',
+    selectAllGroup: 'Seleccionar grupo',
+    deselectAllGroup: 'Desseleccionar grupo'
+};
+
+// link locales
+$.uix.multiselect.i18n['pt_PT'] = $.uix.multiselect.i18n['pt'];
+$.uix.multiselect.i18n['pt_BR'] = $.uix.multiselect.i18n['pt'];
+// ...
+
+})(jQuery);

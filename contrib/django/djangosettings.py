b8c6558d0bcfe39aec1cf2165608d74ba20e4eaa
Johan De Taeye <jdetaeye@frepple.com>
2015-11-13 12:15:09 +0100

    rest api: config update in djangosettings.py

--- contrib/django/djangosettings.py
+++ contrib/django/djangosettings.py
@@ -219,15 +219,19 @@ INSTALLED_APPS = (
 ATTRIBUTES = []
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
+  # Use Django's standard `django.contrib.auth` permissions,
+  # or allow read-only access for unauthenticated users.
+  'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.DjangoModelPermissions'
+  ],
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+  ),
+  'DEFAULT_RENDERER_CLASSES': (
+    'rest_framework.renderers.JSONRenderer',
+    'freppledb.common.api.renderers.freppleBrowsableAPI',
+  )
 }
 
 LOCALE_PATHS = (

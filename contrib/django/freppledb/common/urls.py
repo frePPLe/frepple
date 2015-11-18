0ef495340895e67aae7d6ec9f05ada653761c816
Johan De Taeye <jdetaeye@frepple.com>
2015-11-18 16:28:37 +0100

    Revert "Initial support for 2 factor registration on cloud version"
    This reverts commit 13031672171c7940c2b22281c9043393a9314028.

--- contrib/django/freppledb/common/urls.py
+++ contrib/django/freppledb/common/urls.py
@@ -15,7 +15,7 @@
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns
 
 import freppledb.common.views
 import freppledb.common.serializers
@@ -63,7 +63,4 @@ urlpatterns = patterns(
   (r'^api/common/parameter/(?P<pk>(.+))/$', freppledb.common.serializers.ParameterdetailAPI.as_view()),
   (r'^api/common/comment/(?P<pk>(.+))/$', freppledb.common.serializers.CommentdetailAPI.as_view()),
   (r'^api/$', APIIndexView),
-
- # two factor authentication for cloud
-  (r'^accounts/', include('registration.backends.default.urls')),
 )

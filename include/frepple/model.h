890f531083839a708ad5bee32e05a313b5ad4fcb
Johan De Taeye <jdetaeye@frepple.com>
2015-11-20 12:10:31 +0100

    fix for crashing constraint iterator

--- include/frepple/model.h
+++ include/frepple/model.h
@@ -7103,13 +7103,13 @@ class HasProblems::EntityIterator
 
     /** Used to create an iterator pointing beyond the last HasProblems
       * object. */
-    explicit EntityIterator(unsigned short i) : type(i) {}
+    explicit EntityIterator(unsigned short i) : type(i), bufIter(NULL) {}
 
     /** Copy constructor. */
-    DECLARE_EXPORT EntityIterator(const EntityIterator& o);
+    DECLARE_EXPORT EntityIterator(const EntityIterator&);
 
     /** Assignment operator. */
-    DECLARE_EXPORT EntityIterator& operator=(const EntityIterator& o);
+    DECLARE_EXPORT EntityIterator& operator=(const EntityIterator&);
 
     /** Destructor. */
     DECLARE_EXPORT ~EntityIterator();

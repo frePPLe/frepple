60c76726306e6a5d417ce89a089caca7011b4e63
Johan De Taeye <jdetaeye@frepple.com>
2015-12-12 12:14:22 +0100

    corrected custom 404 handler

--- contrib/django/freppledb/common/views.py
+++ contrib/django/freppledb/common/views.py
@@ -56,7 +56,7 @@ def handler404(request):
     #. Translators: Translation included with Django
     force_text(_('Page not found') + ": " + request.prefix + request.get_full_path())
     )
-  return HttpResponseRedirect(request.prefix + "/admin/")
+  return HttpResponseRedirect(request.prefix + "/data/")
 
 
 def handler500(request):

ff45777e66802aee33f238e19bc33887f862daf4
Johan De Taeye <jdetaeye@frepple.com>
2015-12-23 16:07:35 +0100

    updating release number to 3.1.beta

--- include/frepple/utils.h
+++ include/frepple/utils.h
@@ -120,7 +120,7 @@ using namespace std;
 #include <config.h>
 #else
 // Define the version for (windows) compilers that don't use autoconf
-#define PACKAGE_VERSION "3.0"
+#define PACKAGE_VERSION "3.1.beta"
 #endif
 
 // Header for multithreading

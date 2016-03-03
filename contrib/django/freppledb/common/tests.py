311823b3aeffac01abb0585e9fb723283394fa7d
Johan De Taeye <jdetaeye@frepple.com>
2016-03-03 12:32:52 +0100

    unit test update

--- contrib/django/freppledb/common/tests.py
+++ contrib/django/freppledb/common/tests.py
@@ -43,7 +43,7 @@ class DataLoadTest(TestCase):
     if not isinstance(response, StreamingHttpResponse):
       raise Exception("expected a streaming response")
     for i in response.streaming_content:
-      if b'"records":5,' in i:
+      if b'"records":6,' in i:
         return
     self.fail("Didn't find expected number of parameters")
 

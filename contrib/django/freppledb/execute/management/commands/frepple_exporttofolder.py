9be8382f6a06e00b01cd1a87aae65ab2e8b69a6e
jranita <jranita@frepple.com>
2016-07-29 11:27:40 +0200

    frepple_exporttofolder: executable file

---
+++ contrib/django/freppledb/execute/management/commands/frepple_exporttofolder.py
@@ -0,0 +1,154 @@
+#
+# Copyright (C) 2016 by frePPLe bvba
+#
+# All information contained herein is, and remains the property of frePPLe.
+# You are allowed to use and modify the source code, as long as the software is used
+# within your company.
+# You are not allowed to distribute the software, either in the form of source code
+# or in the form of compiled binaries.
+#
+
+import psycopg2
+import os
+
+from _datetime import datetime
+from optparse import make_option
+from django.conf import settings
+from django.db import DEFAULT_DB_ALIAS
+from django.core.management.base import BaseCommand, CommandError
+
+from freppledb.common.models import User
+from freppledb import VERSION
+from freppledb.execute.models import Task
+from django.contrib.admin.models import LogEntry
+
+class Command(BaseCommand):
+  help = "Exports tables from the frePPLe database to CSV files in a folder"
+  option_list = BaseCommand.option_list + (
+    make_option(
+      '--user', dest='user', type='string',
+      help='User running the command'
+      ),
+    make_option(
+      '--database', action='store', dest='database', default=DEFAULT_DB_ALIAS,
+      help='Nominates a specific database to load the data into'
+      ),
+    make_option(
+      '--task', dest='task', type='int',
+      help='Task identifier (generated automatically if not provided)'
+      )
+  )
+
+  requires_system_checks = False
+  
+  statements = [
+      ("export_purchaseorder.csv", "COPY (select * from purchase_order where status='proposed') TO STDOUT WITH CSV HEADER"),
+      ("export_distributionorder.csv", "COPY distribution_order TO STDOUT WITH CSV HEADER"),
+      ("export_preference.csv", "COPY common_preference TO STDOUT WITH CSV HEADER")     
+      ]
+  
+  def get_version(self):
+    return VERSION
+
+  def handle(self, *args, **options):
+    # Pick up the options
+    if 'database' in options:
+      self.database = options['database'] or DEFAULT_DB_ALIAS
+    else:
+      self.database = DEFAULT_DB_ALIAS
+    if self.database not in settings.DATABASES:
+      raise CommandError("No database settings known for '%s'" % self.database )
+    
+    if 'user' in options and options['user']:
+      try:
+        self.user = User.objects.all().using(self.database).get(username=options['user'])
+      except:
+        raise CommandError("User '%s' not found" % options['user'] )
+    else:
+      self.user = None
+    
+    now = datetime.now()
+
+    task = None
+    self.logfile = None
+    errors = 0
+    try:
+      # Initialize the task
+      if 'task' in options and options['task']:
+        try:
+          task = Task.objects.all().using(self.database).get(pk=options['task'])
+        except:
+          raise CommandError("Task identifier not found")
+        if task.started or task.finished or task.status != "Waiting" or task.name != 'export to folder':
+          raise CommandError("Invalid task identifier")
+        task.status = '0%'
+        task.started = now
+      else:
+        task = Task(name='export to folder', submitted=now, started=now, status='0%', user=self.user)
+      task.arguments = ' '.join(['"%s"' % i for i in args])
+      task.save(using=self.database)
+      
+       # Execute
+      if os.path.isdir(settings.DATABASES[self.database]['FILEUPLOADFOLDER']):
+        
+        # Open the logfile
+        self.logfile = open(os.path.join(settings.DATABASES[self.database]['FILEUPLOADFOLDER'], 'exporttofolder.log'), "a")
+        print("%s Started export to folder\n" % datetime.now(), file=self.logfile)
+        
+        #Define our connection string
+        conn_string = "host='localhost' dbname='Enterprise' user='frepple' password='frepple'"
+
+        conn = psycopg2.connect(conn_string)
+
+        cursor = conn.cursor()
+
+        task.status = '0%'
+        task.save(using=self.database)
+
+        i=0
+        cnt = len(self.statements)
+        
+        for filename, sqlquery in self.statements:
+          print("%s Started export of %s" % (datetime.now(),filename), file=self.logfile)
+
+          try:
+            csv_datafile = open(os.path.join(settings.DATABASES[self.database]['FILEUPLOADFOLDER'], filename), "w")
+            
+            cursor.copy_expert(sqlquery,csv_datafile)
+            
+            csv_datafile.close()
+            i += 1
+
+          except Exception as e:
+            errors += 1
+            print("%s Failed to export to %s" % (datetime.now(),filename), file=self.logfile)
+            if task:
+              task.message = '%s' % e
+            conn = psycopg2.connect(conn_string)
+            cursor = conn.cursor()
+          
+          task.status = str(int(i/cnt*100))+'%'
+          task.save(using=self.database)
+          
+        conn.close()
+        print("%s Exported %s file(s)\n" % (datetime.now(),cnt-errors), file=self.logfile)
+        
+      
+    except Exception as e:
+      print("%s Failed" % datetime.now(), file=self.logfile)
+      errors += 1
+      if task:
+        task.message = '%s' % e
+
+    finally:
+      if task:
+        if not errors:
+          task.status = '100%'
+        else:
+          task.status = 'Failed'
+        task.finished = datetime.now()
+        task.save(using=self.database)
+
+      if self.logfile:
+        print('%s End of export to folder\n' % datetime.now(), file=self.logfile)
+        self.logfile.close()

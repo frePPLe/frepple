1a3703cbb5b2e303ef1f753893b094d6329a671a
jranita <jranita@frepple.com>
2017-07-12 12:06:31 +0200

    frepple.js: fix search link.

--- freppledb/common/static/js/frepple.js
+++ freppledb/common/static/js/frepple.js
@@ -2340,7 +2340,7 @@ $(function() {
         if (data.value === null)
           return '<span><p style="margin-top: 5px; margin-bottom: 1px;">'+data.label+'</p><li  role="separator" class="divider"></li></span>';
         else
-          return '<li><a style="display: block" href="'+ url_prefix + data.url + admin_escape(data.value) + '/" >' + data.value + '</a></li>';
+          return '<li><a style="display: block" href="'+ url_prefix + data.url + admin_escape(data.value) + '/change/" >' + data.value + '</a></li>';
       },
     }
   });

41f2575a36d49c4c566562a2fdf55b04640be860
Johan De Taeye <jdetaeye@frepple.com>
2017-07-27 16:34:55 +0200

    Adding migration to support usernames up to 150 characters

---
+++ freppledb/common/migrations/0011_username.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2017 by frePPLe bvba
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import django.contrib.auth.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+  dependencies = [
+    ('common', '0010_remove_wizard'),
+  ]
+
+  operations = [
+    migrations.AlterField(
+      model_name='user',
+      name='username',
+      field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
+    ),
+  ]

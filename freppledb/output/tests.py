4d9993de606f7cb082dcdbc14536dc8bd169ba74
Johan De Taeye <jdetaeye@frepple.com>
2017-07-27 09:50:13 +0200

    corrected unit test: operation report now only shows only operations with at least one operationplan in the reporting horizon

--- freppledb/output/tests.py
+++ freppledb/output/tests.py
@@ -70,7 +70,7 @@ class OutputTest(TestCase):
   def test_output_operation(self):
     response = self.client.get('/operation/?format=json')
     self.assertEqual(response.status_code, 200)
-    self.assertContains(response, '"records":9,')
+    self.assertContains(response, '"records":3,')
     response = self.client.get('/operation/?format=csvtable')
     self.assertEqual(response.status_code, 200)
     self.assertTrue(response.__getitem__('Content-Type').startswith('text/csv; charset='))

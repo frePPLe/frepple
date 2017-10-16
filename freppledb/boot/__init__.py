ba6194570e0400f094b14b9f51e94e05027cef0d
Johan De Taeye <jdetaeye@frepple.com>
2017-10-16 09:23:33 +0200

    support for read only attributes

--- freppledb/boot/__init__.py
+++ freppledb/boot/__init__.py
@@ -37,23 +37,23 @@ _register = {}
 
 def add_extra_model_fields(sender, **kwargs):
   model_path = "%s.%s" % (sender.__module__, sender.__name__)
-  for field_name, label, fieldtype in _register.get(model_path, []):
+  for field_name, label, fieldtype, editable in _register.get(model_path, []):
     if fieldtype == 'string':
-      field = models.CharField(label, max_length=300, null=True, blank=True, db_index=True)
+      field = models.CharField(label, max_length=300, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'boolean':
-      field = models.NullBooleanField(label, null=True, blank=True, db_index=True)
+      field = models.NullBooleanField(label, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'number':
-      field = models.DecimalField(label, max_digits=15, decimal_places=6, null=True, blank=True, db_index=True)
+      field = models.DecimalField(label, max_digits=15, decimal_places=6, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'integer':
-      field = models.IntegerField(label, null=True, blank=True, db_index=True)
+      field = models.IntegerField(label, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'date':
-      field = models.DateField(label, null=True, blank=True, db_index=True)
+      field = models.DateField(label, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'datetime':
-      field = models.DateTimeField(label, null=True, blank=True, db_index=True)
+      field = models.DateTimeField(label, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'duration':
-      field = models.DurationField(label, null=True, blank=True, db_index=True)
+      field = models.DurationField(label, null=True, blank=True, db_index=True, editable=editable)
     elif fieldtype == 'time':
-      field = models.TimeField(label, null=True, blank=True, db_index=True)
+      field = models.TimeField(label, null=True, blank=True, db_index=True, editable=editable)
     else:
       raise ImproperlyConfigured("Invalid attribute type '%s'." % fieldtype)
     field.contribute_to_class(sender, field_name)
@@ -65,8 +65,13 @@ def registerAttribute(model, attrlist):
   '''
   if model not in _register:
     _register[model] = []
-  for name, label, fieldtype in attrlist:
-    _register[model].append( (name, label, fieldtype, ) )
+  for attr in attrlist:
+    if len(attr) < 3:
+      raise Exception("Invalid attribute definition: %s" % attr)
+    elif len(attr) == 3:
+      _register[model].append(attr + (True,))
+    else:
+      _register[model].append(attr)
 
 
 def getAttributes(model):
@@ -84,28 +89,28 @@ def getAttributeFields(model, related_name_prefix=None, initially_hidden=False):
   from freppledb.common.report import GridFieldInteger, GridFieldDate, GridFieldDateTime
   from freppledb.common.report import GridFieldDuration, GridFieldTime
   result = []
-  for field_name, label, fieldtype in _register.get("%s.%s" % (model.__module__, model.__name__), []):
+  for field_name, label, fieldtype, editable in _register.get("%s.%s" % (model.__module__, model.__name__), []):
     if related_name_prefix:
       field_name = "%s__%s" % (related_name_prefix, field_name)
       label = "%s - %s" % (related_name_prefix.split('__')[-1], label)
     else:
       label = "%s - %s" % (model.__name__, label)
     if fieldtype == 'string':
-      result.append( GridFieldText(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldText(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'boolean':
-      result.append( GridFieldBool(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldBool(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'number':
-      result.append( GridFieldNumber(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldNumber(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'integer':
-      result.append( GridFieldInteger(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldInteger(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'date':
-      result.append( GridFieldDate(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldDate(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'datetime':
-      result.append( GridFieldDateTime(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldDateTime(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'duration':
-      result.append( GridFieldDuration(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldDuration(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     elif fieldtype == 'time':
-      result.append( GridFieldTime(field_name, title=label, initially_hidden=initially_hidden) )
+      result.append( GridFieldTime(field_name, title=label, initially_hidden=initially_hidden, editable=editable) )
     else:
       raise Exception("Invalid attribute type '%s'." % fieldtype)
   return result

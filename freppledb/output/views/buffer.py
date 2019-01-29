146606dd89c0daba72bb01f9910f7461070102c1
Johan De Taeye <jdetaeye@frepple.com>
2019-01-29 10:18:19 +0100

    sorting and filtering of inventory report by buffer

--- freppledb/output/views/buffer.py
+++ freppledb/output/views/buffer.py
@@ -17,8 +17,7 @@
 from datetime import timedelta, datetime
 
 from django.db import connections
-from django.db.models import Value
-from django.db.models.functions import Concat
+from django.db.models.expressions import RawSQL
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import string_concat
@@ -43,9 +42,12 @@ class OverviewReport(GridPivot):
     if len(args) and args[0]:
       return Buffer.objects.all()
     else:
-      return OperationPlanMaterial.objects.values('item','location') \
+      return OperationPlanMaterial.objects.values('item', 'location') \
         .order_by('item_id', 'location_id') \
         .distinct() \
+        .annotate(
+          buffer=RawSQL("item_id || ' @ ' || location_id", ())
+          )
 
   model = OperationPlanMaterial
   default_sort = (1, 'asc', 2, 'asc')

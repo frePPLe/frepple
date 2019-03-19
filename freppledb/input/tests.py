870b33442a68a9e72a64519df7e4657fac33611a
Johan De Taeye <jdetaeye@frepple.com>
2019-03-19 12:13:45 +0100

    extra unit test for MO, PO and DO creation

--- freppledb/input/tests.py
+++ freppledb/input/tests.py
@@ -18,7 +18,8 @@ import tempfile
 
 from django.test import TestCase
 
-from freppledb.input.models import Location
+from freppledb.input.models import Location, Supplier, Item, Operation
+from freppledb.input.models import PurchaseOrder, ManufacturingOrder, DistributionOrder
 
 
 class DataLoadTest(TestCase):
@@ -79,4 +80,64 @@ class DataLoadTest(TestCase):
     self.assertEqual(
       [(i.name, i.category or u'') for i in Location.objects.order_by('name')],
       [(u'All locations', u''), (u'factory 1', u''), (u'factory 2', u''), (u'factory 3', u'cat1'), (u'factory 4', u'')]  # Test result is different in Enterprise Edition
+      )
+
+  def test_forms(self):
+    item = Item.objects.all()[0].name
+    loc1 = Location.objects.all()[0].name
+    loc2 = Location.objects.all()[1].name
+    sup = Supplier.objects.all()[0].name
+    oper = Operation.objects.all()[0].name
+
+    # Create a PO
+    response = self.client.post(
+      "/data/input/purchaseorder/add/", {
+        'reference': "test PO",
+        'item': item,
+        'location': loc1,
+        'supplier': sup,
+        'quantity': 1,
+        'status': 'confirmed'
+      })
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(
+      PurchaseOrder.objects.all().filter(
+        reference="test PO", status="confirmed", quantity=1.0
+        ).count(),
+      1
+      )
+
+    # Create a MO
+    response = self.client.post(
+      "/data/input/manufacturingorder/add/", {
+        'reference': "test MO",
+        'operation': oper,
+        'quantity': 1,
+        'status': 'confirmed'
+      })
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(
+      ManufacturingOrder.objects.all().filter(
+        reference="test MO", status="confirmed", quantity=1.0, operation=oper
+        ).count(),
+      1
+      )
+
+    # Create a DO
+    response = self.client.post(
+      "/data/input/distributionorder/add/", {
+        'reference': "test DO",
+        'origin': loc1,
+        'destination': loc2,
+        'quantity': 1,
+        'status': 'confirmed',
+        'shipping_date_0': '2019-01-01',
+        'shipping_date_1': '00:00:00'
+      })
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(
+      DistributionOrder.objects.all().filter(
+        reference="test DO", status="confirmed", quantity=1.0, origin=loc1, destination=loc2
+        ).count(),
+      1
       )

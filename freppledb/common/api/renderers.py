1884a99f99c696af3236a7cd78824d2ab5d07f81
Johan De Taeye <jdetaeye@frepple.com>
2019-03-27 13:05:06 +0100

    rest api: workaround for a http-500 error

--- freppledb/common/api/renderers.py
+++ freppledb/common/api/renderers.py
@@ -17,6 +17,7 @@
 
 from rest_framework.renderers import BrowsableAPIRenderer
 
+
 class freppleBrowsableAPI(BrowsableAPIRenderer):
   '''
   Customized rendered for the browsable API:
@@ -30,3 +31,9 @@ class freppleBrowsableAPI(BrowsableAPIRenderer):
     else:
       ctx['title'] = "Detail API for %s" % ctx['name']
     return ctx
+
+  def render(self, data, accepted_media_type=None, renderer_context=None):
+    if isinstance(data, list) and renderer_context['request'].method in ('POST', 'PUT', 'PATCH'):
+      # Workaround for a 500-error when posting a list update in the user interface
+      data = data[0]
+    return super().render(data, accepted_media_type, renderer_context)

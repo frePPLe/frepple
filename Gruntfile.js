e79b37c7238814793015aa2afaaa90df9477ec7b
Johan De Taeye <jdetaeye@frepple.com>
2019-07-11 09:00:39 +0200

    updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-angular-gettext');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-exec');
 

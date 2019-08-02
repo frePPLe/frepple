849e8f3d89b4ef228c314cb3c14ab535657f7418
Johan De Taeye <jdetaeye@frepple.com>
2019-08-02 16:27:53 +0200

    guided tour: make the dialog moveable

--- freppledb/common/static/js/frepple.js
+++ freppledb/common/static/js/frepple.js
@@ -3045,7 +3045,8 @@ var tour = {
     .on('shown.bs.modal', function () {
       tour.showStep();
     })
-    .modal('show');
+    .modal('show')
+    .drags({handle: "div.modal-header"});
     $('#tourmodalbody').append( '<div id="tour" style="padding-bottom:20px; display:none">' +
          tourdata[tour.chapter]['description'].replace(/{url_prefix}/g, url_prefix) + '<br><br><br></div>');
   },
@@ -3344,4 +3345,47 @@ function showModalImage(event, title) {
 	popup.find("img").attr("src", $(event.target).attr("src"));
   popup.modal('show');
 	event.preventDefault();
+}
+
+/* Jquery extension to make an element draggable. 
+ * Copied from http://cssdeck.com/labs/wsse5ous
+ */
+$.fn.drags = function(opt) {
+
+  opt = $.extend({handle:"",cursor:"move"}, opt);
+
+  if(opt.handle === "") {
+      var $el = this;
+  } else {
+      var $el = this.find(opt.handle);
+  }
+
+  return $el
+    .css('cursor', opt.cursor)
+    .on("mousedown", function(e) {
+      if(opt.handle === "")
+          var $drag = $(this).addClass('draggable');
+      else
+          var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+      var z_idx = $drag.css('z-index'),
+          drg_h = $drag.outerHeight(),
+          drg_w = $drag.outerWidth(),
+          pos_y = $drag.offset().top + drg_h - e.pageY,
+          pos_x = $drag.offset().left + drg_w - e.pageX;
+      $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+          $('.draggable').offset({
+              top:e.pageY + pos_y - drg_h,
+              left:e.pageX + pos_x - drg_w
+          }).on("mouseup", function() {
+              $(this).removeClass('draggable').css('z-index', z_idx);
+          });
+      });
+      e.preventDefault(); // disable selection
+  }).on("mouseup", function() {
+      if(opt.handle === "") {
+          $(this).removeClass('draggable');
+      } else {
+          $(this).removeClass('active-handle').parent().removeClass('draggable');
+      }
+  });
 }

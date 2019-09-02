248adb954c52e3b968c6b99e476682aabe922e99
Johan De Taeye <jdetaeye@frepple.com>
2019-09-02 18:10:18 +0200

    Cleaner message to users on the login form

--- freppledb/admin.py
+++ freppledb/admin.py
@@ -19,9 +19,25 @@ from importlib import import_module
 
 from django.conf import settings
 from django.contrib.admin.sites import AdminSite, AlreadyRegistered
+from django.contrib.admin.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
+
+class freppleAuthenticationForm(AuthenticationForm):
+    error_messages = {
+        # . Translators: Translation included with Django
+        "invalid_login": _(
+            "Please enter a correct %(username)s and password. Note that both "
+            "fields may be case-sensitive."
+        ),
+        "inactive": _("This user is inactive."),
+    }
 
 
 class freppleAdminSite(AdminSite):
+
+    login_form = freppleAuthenticationForm
+
     def register(self, model_or_iterable, admin_class=None, force=False, **options):
         try:
             super().register(model_or_iterable, admin_class, **options)

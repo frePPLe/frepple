fde709720bf0203bdf2e59792333f4e82d0afbc1
Jiří Kuneš <jiri.kunes@orgis.cz>
2020-04-20 14:11:31 +0200

    fixed odoo (v12,v13) action registry subclass

--- contrib/odoo/addons_v13/frepple/static/src/js/frepple.js
+++ contrib/odoo/addons_v13/frepple/static/src/js/frepple.js
@@ -2,14 +2,13 @@ odoo.define('frepple', function (require) {
   'use strict';
 
   var core = require('web.core');
-  var Widget = require('web.Widget');
+  var AbstractAction = require('web.AbstractAction');
 
-  /* Forecast editor widget. */
-  var ForecastEditor = Widget.extend({
-    canBeRemoved: function () {
-      return $.when();
-      },
+  /* Forecast editor page. */
+  var ForecastEditor = AbstractAction.extend({
     start: function() {
+      this._super.apply(this, arguments);
+
       var el = this.$el;
       el.height("calc(100% - 34px)");
       this._rpc({
@@ -22,19 +21,15 @@ odoo.define('frepple', function (require) {
             + '" width="100%" height="100%" marginwidth="0" marginheight="0" frameborder="no" '
             + ' scrolling="yes" style="border-width:0px;"/>');
           });
-    },
-    getTitle: function () {
-        return "Forecast Editor";
     }
   });
   core.action_registry.add('frepple.forecasteditor', ForecastEditor);
 
-  /* Inventory planning widget. */
-  var InventoryPlanning = Widget.extend({
-    canBeRemoved: function () {
-      return $.when();
-      },  	
+  /* Inventory planning page. */
+  var InventoryPlanning = AbstractAction.extend({
     start: function() {
+      this._super.apply(this, arguments);
+
       var el = this.$el;
       el.height("calc(100% - 34px)");
       this._rpc({
@@ -47,19 +42,15 @@ odoo.define('frepple', function (require) {
             + '" width="100%" height="100%" marginwidth="0" marginheight="0" frameborder="no" '
             + ' scrolling="yes" style="border-width:0px;"/>');
           });
-    },
-    getTitle: function () {
-        return "Inventory Planning";
     }
   });
   core.action_registry.add('frepple.inventoryplanning', InventoryPlanning);
 
-  /* Plan editor widget. */
-  var PlanEditor = Widget.extend({
-    canBeRemoved: function () {
-      return $.when();
-      },  	
+  /* Plan editor page. */
+  var PlanEditor = AbstractAction.extend({
     start: function() {
+      this._super.apply(this, arguments);
+
       var el = this.$el;
       el.height("calc(100% - 34px)");
       this._rpc({
@@ -72,36 +63,35 @@ odoo.define('frepple', function (require) {
             + '" width="100%" height="100%" marginwidth="0" marginheight="0" frameborder="no" '
             + ' scrolling="yes" style="border-width:0px;"/>');
           });
-    },
-    getTitle: function () {
-        return "Plan Editor";
     }
   });
   core.action_registry.add('frepple.planeditor', PlanEditor);
 
-  /* Full user interface widget. */
-  var HomePage = Widget.extend({
-    canBeRemoved: function () {
-      return $.when();
-      },
+  /* Full user interface page. */
+  var HomePage = AbstractAction.extend({
     start: function() {
+      this._super.apply(this, arguments);
+
       var el = this.$el;
       el.height("calc(100% - 34px)");
       this._rpc({
         model: 'res.company',
         method: 'getFreppleURL',
         args: [true, '/'],
-        })      
+        })
         .then(function(result) {
           el.append('<iframe src="' + result
             + '" width="100%" height="100%" marginwidth="0" marginheight="0" frameborder="no" '
             + ' scrolling="yes" style="border-width:0px;"/>');
           });
-    },
-    getTitle: function () {
-        return "frePPLe";
     }
   });
   core.action_registry.add('frepple.homepage', HomePage);
 
+  return {
+    'frepple.forecasteditor': ForecastEditor,
+    'frepple.inventoryplanning': InventoryPlanning,
+    'frepple.planeditor': PlanEditor,
+    'frepple.homepage': HomePage,
+  };
 });

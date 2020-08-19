f66c840d89ff7369152c034ed99b1d0e46385547
Johan De Taeye <jdetaeye@frepple.com>
2020-08-19 09:35:19 +0200

    make version number compliant with pep-0396

--- freppledb/__init__.py
+++ freppledb/__init__.py
@@ -1,19 +1,32 @@
-r"""
-A Django project implementing a web-based user interface for frePPLe.
-"""
-
+#
+# Copyright (C) 2020 by frePPLe bv
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 VERSION = "6.7.0"
+__version__ = VERSION
 
 
 def runCommand(taskname, *args, **kwargs):
     """
-  Auxilary method to run a django command. It is intended to be used
-  as a target for the multiprocessing module.
+    Auxilary method to run a django command. It is intended to be used
+    as a target for the multiprocessing module.
 
-  The code is put here, such that a child process loads only
-  a minimum of other python modules.
-  """
+    The code is put here, such that a child process loads only
+    a minimum of other python modules.
+    """
     # Initialize django
     import os
 

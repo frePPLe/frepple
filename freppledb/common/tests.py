c4eed55459b0a2adf10d7f1166f1746acee817ce
Johan De Taeye <jdetaeye@frepple.com>
2020-11-30 09:41:12 +0100

    experiment with selenium unit test class

--- freppledb/common/tests.py
+++ freppledb/common/tests.py
@@ -15,10 +15,28 @@
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import time
+import unittest
+
+from django.conf import settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.db import DEFAULT_DB_ALIAS
 from django.http.response import StreamingHttpResponse
 from django.test import TestCase
 
 from freppledb.common.models import User
+
+try:
+    from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions
+
+    noSelenium = False
+except ImportError:
+    noSelenium = True
 
 
 def checkResponse(testcase, response):
@@ -28,6 +46,51 @@ def checkResponse(testcase, response):
     testcase.assertEqual(response.status_code, 200)
 
 
+class SeleniumTest(StaticLiveServerTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        if False:
+            firefox_options = webdriver.FirefoxOptions()
+            # firefox_options.add_argument("--headless")
+            cls.driver = webdriver.Firefox(firefox_options=firefox_options)
+        else:
+            options = webdriver.ChromeOptions()
+            options.add_argument("--silent")
+            # options.add_argument("--headless")
+            cls.driver = webdriver.Chrome(chrome_options=options)
+        cls.driver.set_window_size(1080, 800)
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.driver.quit()
+        time.sleep(2)
+
+    def open(self, url):
+        return self.driver.get("%s%s" % (self.live_server_url, url))
+
+    def login(self, user, password):
+        if (
+            "freppledb.common.middleware.AutoLoginAsAdminUser"
+            not in settings.MIDDLEWARE
+        ):
+            self.findElement(By.NAME, "username").send_keys(user)
+            self.findElement(By.NAME, "password").send_keys(password)
+            self.findElement(By.CSS_SELECTOR, "[type='submit']").click()
+
+    def findElement(self, by, locator):
+        try:
+            return self.driver.find_element(by, locator)
+        except NoSuchElementException:
+            self.fail("Element %s not found by %s" % (locator, by))
+
+    def ActionChains(self):
+        return ActionChains(self.driver)
 class DataLoadTest(TestCase):
     def setUp(self):
         # Login

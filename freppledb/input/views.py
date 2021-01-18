9e29488424dfb011dfdf7c133dbf6b99071f9fab
hichamlahlou <hlahlou@frepple.com>
2021-01-18 17:59:33 +0100

    Add item and location attributes in the item supplier table.

--- freppledb/input/views.py
+++ freppledb/input/views.py
@@ -2197,6 +2197,24 @@ class ItemSupplierList(GridReport):
         ),
     )
 
+    @classmethod
+    def initialize(reportclass, request):
+        if reportclass._attributes_added != 2:
+            reportclass._attributes_added = 2
+            reportclass.attr_sql = ""
+            # Adding custom item attributes
+            for f in getAttributeFields(
+                Item, related_name_prefix="item", initially_hidden=True
+            ):
+                reportclass.rows += (f,)
+                reportclass.attr_sql += "item.%s, " % f.name.split("__")[-1]
+            # Adding custom location attributes
+            for f in getAttributeFields(
+                Location, related_name_prefix="location", initially_hidden=True
+            ):
+                reportclass.rows += (f,)
+                reportclass.attr_sql += "location.%s, " % f.name.split("__")[-1]
+
 
 class ItemDistributionList(GridReport):
     title = _("item distributions")

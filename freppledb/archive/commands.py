a312eab0c4a6dff141a743a10a97f2590a99d535
Johan De Taeye <jdetaeye@frepple.com>
2021-03-26 09:08:33 +0100

    only archive when a supply plan is included in the planning run.  Otherwise we risk to archive an incomplete plan.

--- freppledb/archive/commands.py
+++ freppledb/archive/commands.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import os
 
 from django.db import DEFAULT_DB_ALIAS
 
@@ -27,7 +28,10 @@ class archiveData(PlanTask):
 
     @classmethod
     def getWeight(cls, database=DEFAULT_DB_ALIAS, **kwargs):
-        return 1
+        if "supply" in os.environ:
+            return 1
+        else:
+            return -1
 
     @classmethod
     def run(cls, database=DEFAULT_DB_ALIAS, **kwargs):

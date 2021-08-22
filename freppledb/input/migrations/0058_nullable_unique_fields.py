97f2a85d1236051edb851a6a7cfa48b7293fe503
Johan De Taeye <jdetaeye@frepple.com>
2021-08-22 09:39:17 +0200

    migration updated to delete duplicates before activating unique constraint

--- freppledb/input/migrations/0058_nullable_unique_fields.py
+++ freppledb/input/migrations/0058_nullable_unique_fields.py
@@ -26,10 +26,41 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
+            """
+            delete from buffer a
+            using (
+                select max(id) as id, item_id, location_id, batch
+                from buffer 
+                group by item_id, location_id, batch
+                having count(*) > 1
+                ) b
+            where a.item_id is not distinct from b.item_id
+            and a.location_id is not distinct from b.location_id
+            and a.batch is not distinct from b.batch
+            and a.id is distinct from b.id
+            """
+        ),
+        migrations.RunSQL(
             "update buffer set batch = '' where batch is null",
         ),
         migrations.RunSQL(
             "ALTER TABLE buffer ALTER COLUMN batch SET DEFAULT ''",
+        ),
+        migrations.RunSQL(
+            """
+            delete from calendarbucket a
+            using (
+                select max(id) as id, calendar_id, priority, startdate, enddate
+                from calendarbucket 
+                group by calendar_id, priority, startdate, enddate
+                having count(*) > 1
+                ) b
+            where a.calendar_id is not distinct from b.calendar_id
+            and a.priority is not distinct from b.priority
+            and a.startdate is not distinct from b.startdate
+            and a.enddate is not distinct from b.enddate
+            and a.id is distinct from b.id
+            """
         ),
         migrations.RunSQL(
             "update calendarbucket set startdate = '1971-01-01'::timestamp where startdate is null",
@@ -50,16 +81,61 @@ class Migration(migrations.Migration):
             "ALTER TABLE calendarbucket ALTER COLUMN priority SET DEFAULT 0",
         ),
         migrations.RunSQL(
+            """
+            delete from suboperation a
+            using (
+                select max(id) as id, operation_id, suboperation_id, effective_start
+                from suboperation 
+                group by operation_id, suboperation_id, effective_start
+                having count(*) > 1
+                ) b
+            where a.operation_id is not distinct from b.operation_id
+            and a.suboperation_id is not distinct from b.suboperation_id
+            and a.effective_start is not distinct from b.effective_start
+            and a.id is distinct from b.id
+            """
+        ),
+        migrations.RunSQL(
             "update suboperation set effective_start = '1971-01-01'::timestamp where effective_start is null",
         ),
         migrations.RunSQL(
             "ALTER TABLE suboperation ALTER COLUMN effective_start SET DEFAULT '1971-01-01'::timestamp",
         ),
         migrations.RunSQL(
+            """
+            delete from suboperation a
+            using (
+                select max(id) as id, operation_id, suboperation_id, effective_start
+                from suboperation 
+                group by operation_id, suboperation_id, effective_start
+                having count(*) > 1
+                ) b
+            where a.operation_id is not distinct from b.operation_id
+            and a.suboperation_id is not distinct from b.suboperation_id
+            and a.effective_start is not distinct from b.effective_start
+            and a.id is distinct from b.id
+            """
+        ),
+        migrations.RunSQL(
             "update operationmaterial set effective_start = '1971-01-01'::timestamp where effective_start is null",
         ),
         migrations.RunSQL(
             "ALTER TABLE operationmaterial ALTER COLUMN effective_start SET DEFAULT '1971-01-01'::timestamp",
+        ),
+        migrations.RunSQL(
+            """
+            delete from operationresource a
+            using (
+                select max(id) as id, operation_id, resource_id, effective_start
+                from operationresource 
+                group by operation_id, resource_id, effective_start
+                having count(*) > 1
+                ) b
+            where a.operation_id is not distinct from b.operation_id
+            and a.resource_id is not distinct from b.resource_id
+            and a.effective_start is not distinct from b.effective_start
+            and a.id is distinct from b.id
+            """
         ),
         migrations.RunSQL(
             "update operationresource set effective_start = '1971-01-01'::timestamp where effective_start is null",

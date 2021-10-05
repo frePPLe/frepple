fe91a6ddd76d1037453b8eb45e03b7ef009b6c09
Johan De Taeye <jdetaeye@frepple.com>
2021-10-05 16:50:34 +0200

    revised migration for uom field

--- freppledb/input/migrations/0059_item_uom.py
+++ freppledb/input/migrations/0059_item_uom.py
@@ -14,7 +14,53 @@
 # License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from django.db import migrations, models
+from django.db import migrations, models, connections
+
+
+def rename_existing_columns(apps, schema_editor):
+    db = schema_editor.connection.alias
+    with connections[db].cursor() as cursor:
+        cursor.execute(
+            """
+            select count(*)
+            from information_schema.columns
+            where table_name='item' and column_name='uom'
+            """
+        )
+        if cursor.fetchone()[0]:
+            cursor.execute("alter table item rename column volume to original_uom")
+
+
+def copy_existing_data(apps, schema_editor):
+    db = schema_editor.connection.alias
+    with connections[db].cursor() as cursor:
+        cursor.execute(
+            """
+            select column_name
+            from information_schema.columns
+            where table_name='item'
+            and column_name='original_uom'
+            """
+        )
+        cols = ["%s=%s" % (c[0][9:], c[0]) for c in cursor.fetchall()]
+        if cols:
+            cursor.execute("update item set %s" % ", ".join(cols))
+
+
+def drop_temp_columns(apps, schema_editor):
+    db = schema_editor.connection.alias
+    with connections[db].cursor() as cursor:
+        cursor.execute(
+            """
+            select column_name
+            from information_schema.columns
+            where table_name='item'
+            and column_name='original_uom'
+            """
+        )
+        cols = ["drop column %s" % c[0] for c in cursor.fetchall()]
+        if cols:
+            cursor.execute("alter table item %s" % ", ".join(cols))
 
 
 class Migration(migrations.Migration):
@@ -24,6 +70,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(rename_existing_columns),
         migrations.AddField(
             model_name="item",
             name="uom",
@@ -31,4 +78,6 @@ class Migration(migrations.Migration):
                 blank=True, max_length=20, null=True, verbose_name="unit of measure"
             ),
         ),
+        migrations.RunPython(copy_existing_data),
+        migrations.RunPython(drop_temp_columns),
     ]

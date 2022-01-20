2417c7a6e9074b80f2c5ff6c7891d452210a5ba2
Hicham Lahlou <hlahlou@frepple.com>
2022-01-20 17:35:22 +0100

    clean the migration file

--- freppledb/input/migrations/0061_itemsupplier_uniqueness.py
+++ freppledb/input/migrations/0061_itemsupplier_uniqueness.py
@@ -21,10 +21,25 @@ def dropExistingIndex(apps, schema_editor):
     db = schema_editor.connection.alias
     output = []
     with connections[db].cursor() as cursor:
+        # delete records that will fail with the new indexes
+        cursor.execute(
+            """
+            with cte as
+            (
+            select id, row_number() over(partition by item_id, supplier_id, effective_start order by cost) as rn
+            from itemsupplier
+            where location_id is null
+            )
+            delete from itemsupplier
+            using cte
+            where itemsupplier.id = cte.id
+            and cte.rn > 1;
+        """
+        )
         cursor.execute(
             """
             select
-                i.relname as index_name
+                i.relname as index_name, array_agg(a.attname)
             from
                 pg_class t,
                 pg_class i,
@@ -37,64 +52,23 @@ def dropExistingIndex(apps, schema_editor):
                 and a.attnum = ANY(ix.indkey)
                 and t.relkind = 'r'
                 and t.relname like 'itemsupplier'
-                and a.attname = 'item_id'
                 and ix.indisunique
-            intersect
-            select
-                i.relname as index_name
-            from
-                pg_class t,
-                pg_class i,
-                pg_index ix,
-                pg_attribute a
-            where
-                t.oid = ix.indrelid
-                and i.oid = ix.indexrelid
-                and a.attrelid = t.oid
-                and a.attnum = ANY(ix.indkey)
-                and t.relkind = 'r'
-                and t.relname like 'itemsupplier'
-                and a.attname = 'location_id'
-                and ix.indisunique
-            intersect
-            select
-                i.relname as index_name
-            from
-                pg_class t,
-                pg_class i,
-                pg_index ix,
-                pg_attribute a
-            where
-                t.oid = ix.indrelid
-                and i.oid = ix.indexrelid
-                and a.attrelid = t.oid
-                and a.attnum = ANY(ix.indkey)
-                and t.relkind = 'r'
-                and t.relname like 'itemsupplier'
-                and a.attname = 'supplier_id'
-                and ix.indisunique
-            intersect
-            select
-                i.relname as index_name
-            from
-                pg_class t,
-                pg_class i,
-                pg_index ix,
-                pg_attribute a
-            where
-                t.oid = ix.indrelid
-                and i.oid = ix.indexrelid
-                and a.attrelid = t.oid
-                and a.attnum = ANY(ix.indkey)
-                and t.relkind = 'r'
-                and t.relname like 'itemsupplier'
-                and a.attname = 'effective_start'
-                and ix.indisunique
+            group by i.relname
             """
         )
         for i in cursor:
-            output.append(i[0])
-        cursor.execute("alter table itemsupplier drop constraint %s" % ",".join(output))
+            if (
+                "item_id" in i[1]
+                and "location_id" in i[1]
+                and "supplier_id" in i[1]
+                and "effective_start" in i[1]
+            ):
+
+                output.append(i[0])
+        if len(output) > 0:
+            cursor.execute(
+                "alter table itemsupplier drop constraint %s" % ",".join(output)
+            )
 
 
 class Migration(migrations.Migration):

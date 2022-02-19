6d5e85e9ef55a2df5a8956050793f37fea1e0a65
Johan De Taeye <jdetaeye@frepple.com>
2022-02-19 17:38:23 +0100

    django 3.2 update

--- freppledb/common/templatetags.py
+++ freppledb/common/templatetags.py
@@ -20,11 +20,24 @@ from decimal import Decimal
 import json
 from pathlib import Path
 
+from bootstrap3 import renderers
+from bootstrap3.utils import add_css_class
+from bootstrap3.text import text_value
+
+
 from django.apps import apps
-from django.contrib.admin.utils import unquote, quote
-from django.template import Library, Node, Variable, TemplateSyntaxError
 from django.conf import settings
+from django.contrib.admin.utils import unquote, quote
+from django.contrib.auth.forms import ReadOnlyPasswordHashWidget
+from django.contrib.admin.widgets import (
+    AdminDateWidget,
+    AdminTimeWidget,
+    AdminSplitDateTime,
+    RelatedFieldWidgetWrapper,
+)
 from django.db import models, connections
+from django.forms import FileInput, CheckboxInput, RadioSelect, CheckboxSelectMultiple
+from django.template import Library, Node, Variable, TemplateSyntaxError
 from django.utils.translation import gettext as _
 from django.utils.http import urlquote
 from django.utils.encoding import iri_to_uri, force_str
@@ -781,3 +794,50 @@ timeformatfilter.is_safe = True
 @register.filter(name="extension")
 def extensionfilter(val):
     return Path(val).suffix[1:].lower()
+
+
+@register.simple_tag(name="render_field_bootstrap")
+def render_field_bootstrap(field, **kwargs):
+    class BootstrapFieldRenderer(renderers.FieldRenderer):
+        """
+        A django-bootstrap3 field renderer that renders just the field
+        """
+
+        def render(self):
+            # Hidden input requires no special treatment
+            if self.field.is_hidden:
+                return text_value(self.field)
+            # Render the widget
+            self.add_widget_attrs()
+            html = self.field.as_widget(attrs=self.widget.attrs)
+            return html
+
+        def add_class_attrs(self, widget=None):
+            if not widget:
+                widget = self.widget
+
+            # for multiwidgets we recursively update classes for each sub-widget
+            if isinstance(widget, AdminSplitDateTime):
+                for w in widget.widgets:
+                    self.add_class_attrs(w)
+                return
+
+            classes = widget.attrs.get("class", "")
+            if isinstance(widget, ReadOnlyPasswordHashWidget):
+                classes = add_css_class(classes, "form-control-static", prepend=True)
+            elif isinstance(
+                widget, (AdminDateWidget, AdminTimeWidget, RelatedFieldWidgetWrapper)
+            ):
+                # for some admin widgets we don't want the input to take full horizontal space
+                classes = add_css_class(
+                    classes, "form-control form-control-inline", prepend=True
+                )
+            elif not isinstance(
+                widget, (CheckboxInput, RadioSelect, CheckboxSelectMultiple, FileInput)
+            ):
+                classes = add_css_class(classes, "form-control", prepend=True)
+                # For these widget types, add the size class here
+                classes = add_css_class(classes, self.get_size_class())
+            widget.attrs["class"] = classes
+
+    return BootstrapFieldRenderer(field, **kwargs).render()

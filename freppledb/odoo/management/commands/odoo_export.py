6fea2d1d16c385b9d7e9b6debb76a6529d3db1f0
Johan De Taeye <jdetaeye@frepple.com>
2022-06-13 18:02:47 +0200

    odoo bulk export updates

--- freppledb/odoo/management/commands/odoo_export.py
+++ freppledb/odoo/management/commands/odoo_export.py
@@ -17,12 +17,12 @@
 import base64
 from datetime import datetime, timedelta
 import email
+import json
 import jwt
 import time
 from xml.sax.saxutils import quoteattr
 from urllib.request import urlopen, Request
 
-from django.core import management
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.db import DEFAULT_DB_ALIAS
@@ -240,6 +240,7 @@ class Command(BaseCommand):
                 startdate__lte=today + timedelta(days=7),
             )
             .order_by("startdate")
+            .select_related("location", "item", "supplier")
         ):
             if (
                 i.status not in ("proposed", "approved")
@@ -248,11 +249,12 @@ class Command(BaseCommand):
                 or not i.item.subcategory
                 or not i.location.subcategory
                 or not i.item.source.startswith("odoo")
+                or i.supplier.name == "Unknown supplier"
             ):
                 continue
             self.exported.append(i)
-            yield '<operationplan reference="%s" ordertype="PO" item=%s location=%s supplier=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" batch=%s/>' % (
-                i.reference,
+            yield '<operationplan reference=%s ordertype="PO" item=%s location=%s supplier=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" batch=%s/>' % (
+                quoteattr(i.reference),
                 quoteattr(i.item.name),
                 quoteattr(i.location.name),
                 quoteattr(i.supplier.name),
@@ -274,43 +276,47 @@ class Command(BaseCommand):
                 startdate__lte=today + timedelta(days=7),
             )
             .order_by("startdate")
+            .select_related("item", "origin", "destination")
         ):
             if (
                 i.status not in ("proposed", "approved")
                 or not i.item
                 or not i.item.source
                 or not i.item.subcategory
-                or not i.operation.origin.location.subcategory
-                or not i.operation.destination.location.subcategory
+                or not i.origin.subcategory
+                or not i.destination.subcategory
                 or not i.item.source.startswith("odoo")
             ):
                 continue
             self.exported.append(i)
-            yield '<operationplan status="%s" reference="%s" ordertype="DO" item=%s origin=%s destination=%s start="%s" end="%s" quantity="%s" origin_id=%s destination_id=%s item_id=%s criticality="%d" batch=%s/>' % (
+            yield '<operationplan status="%s" reference=%s ordertype="DO" item=%s origin=%s destination=%s start="%s" end="%s" quantity="%s" origin_id=%s destination_id=%s item_id=%s criticality="%d" batch=%s/>' % (
                 i.status,
-                i.reference,
-                quoteattr(i.operation.destination.item.name),
-                quoteattr(i.operation.origin.location.name),
-                quoteattr(i.operation.destination.location.name),
+                quoteattr(i.reference),
+                quoteattr(i.item.name),
+                quoteattr(i.origin.name),
+                quoteattr(i.destination.name),
                 i.startdate,
                 i.enddate,
                 i.quantity,
-                quoteattr(i.operation.origin.location.subcategory),
-                quoteattr(i.operation.destination.location.subcategory),
-                quoteattr(i.operation.destination.item.subcategory),
+                quoteattr(i.origin.subcategory),
+                quoteattr(i.destination.subcategory),
+                quoteattr(i.item.subcategory),
                 int(i.criticality),
                 quoteattr(i.batch or ""),
             )
 
-        # Manufacturing orders and work orders to export
+        # Manufacturing orders to export
         for i in (
             ManufacturingOrder.objects.using(self.database)
             .filter(
-                status="proposed",
                 item__source__startswith="odoo",
+                operation__source__startswith="odoo",
+                operation__owner__isnull=True,
+                status="proposed",
                 startdate__lte=today + timedelta(days=7),
             )
             .order_by("startdate")
+            .select_related("operation", "location", "item", "owner")
         ):
             if (
                 i.status not in ("proposed", "approved")
@@ -318,27 +324,21 @@ class Command(BaseCommand):
                 or not i.operation.source
                 or not i.operation.item
                 or not i.operation.source.startswith("odoo")
-                or not i.operation.item.subcategory
-                or not i.operation.location.subcategory
+                or not i.item.subcategory
+                or not i.location.subcategory
             ):
                 continue
             self.exported.append(i)
             res = set()
             try:
-                for j in i.loadplans:
+                for j in i.operationplanresources:
                     res.add(j.resource.name)
             except Exception:
                 pass
-            demand = {}
-            demand_str = ""
-            for d in i.pegging_demand:
-                demand[d.demand] = d.quantity
-                demand_str += "%s:%s, " % (d.demand, d.quantity)
-            if demand_str:
-                demand_str = demand_str[:-2]
+            demand_str = json.dumps(i.plan["pegging"]) if i.plan["pegging"] else ""
             if i.operation.category == "subcontractor":
-                yield '<operationplan ordertype="PO" id="%s" item=%s location=%s supplier=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" batch=%s/>' % (
-                    i.reference,
+                yield '<operationplan ordertype="PO" id=%s item=%s location=%s supplier=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" batch=%s/>' % (
+                    quoteattr(i.reference),
                     quoteattr(i.item.name),
                     quoteattr(i.location.name),
                     quoteattr(i.operation.subcategory or ""),
@@ -351,8 +351,8 @@ class Command(BaseCommand):
                     quoteattr(i.batch or ""),
                 )
             else:
-                yield '<operationplan reference="%s" ordertype="MO" item=%s location=%s operation=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" resource=%s demand=%s batch=%s/>' % (
-                    i.reference,
+                yield '<operationplan reference=%s ordertype="MO" item=%s location=%s operation=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s criticality="%d" resource=%s demand=%s batch=%s/>' % (
+                    quoteattr(i.reference),
                     quoteattr(i.operation.item.name),
                     quoteattr(i.operation.location.name),
                     quoteattr(i.operation.name),
@@ -366,6 +366,53 @@ class Command(BaseCommand):
                     quoteattr(demand_str),
                     quoteattr(i.batch or ""),
                 )
+
+        # Work orders to export
+        # Normally we don't create work orders, but only updates existing work orders.
+        # We leave it to odoo to create the workorders for a manufacturing order.
+        for i in (
+            ManufacturingOrder.objects.using(self.database)
+            .filter(
+                owner__operation__type="routing",
+                operation__source__startswith="odoo",
+                owner__item__source__startswith="odoo",
+                status__in=("proposed", "approved"),
+                startdate__lte=today + timedelta(days=7),
+            )
+            .order_by("startdate")
+            .select_related("operation", "location", "item", "owner")
+        ):
+            if (
+                i.status not in ("proposed", "approved")
+                or not i.operation
+                or not i.operation.source
+                or not i.owner.operation.item
+                or not i.operation.source.startswith("odoo")
+                or not i.owner.item.subcategory
+                or not i.location.subcategory
+            ):
+                continue
+            self.exported.append(i)
+            res = set()
+            try:
+                for j in i.operationplanresources:
+                    res.add(j.resource.name)
+            except Exception:
+                pass
+            yield '<operationplan reference=%s owner=%s ordertype="WO" item=%s location=%s operation=%s start="%s" end="%s" quantity="%s" location_id=%s item_id=%s resource=%s batch=%s/>' % (
+                quoteattr(i.reference),
+                quoteattr(i.owner.reference),
+                quoteattr(i.owner.operation.item.name),
+                quoteattr(i.operation.location.name),
+                quoteattr(i.operation.name),
+                i.startdate,
+                i.enddate,
+                i.quantity,
+                quoteattr(i.operation.location.subcategory),
+                quoteattr(i.owner.operation.item.subcategory),
+                quoteattr(",".join(res)),
+                quoteattr(i.batch or ""),
+            )
 
         # Write the footer
         yield "</operationplans>"

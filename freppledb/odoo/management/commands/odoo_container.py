03d81b47bba74fb35394d864216f480868ca9dfe
Johan De Taeye <jdetaeye@frepple.com>
2022-08-04 16:22:37 +0200

    odoo: also install freppledata module in the odoo container

--- freppledb/odoo/management/commands/odoo_container.py
+++ freppledb/odoo/management/commands/odoo_container.py
@@ -146,7 +146,7 @@ class Command(BaseCommand):
                     "-t",
                     name,
                     "odoo",
-                    "--init=base,product,purchase,sale,sale_management,resource,mrp,frepple,autologin",
+                    "--init=base,product,purchase,sale,sale_management,resource,stock,mrp,frepple,freppledata,autologin",
                     "--load=web,autologin",
                     "--database=%s" % name,
                     "--stop-after-init",

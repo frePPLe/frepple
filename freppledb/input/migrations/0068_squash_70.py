a242032100a2a0bf910ea8df89798b754c80af0a
Johan De Taeye <jdetaeye@frepple.com>
2022-12-01 12:40:12 +0100

    query optimization to speed up migrations

--- freppledb/input/migrations/0068_squash_70.py
+++ freppledb/input/migrations/0068_squash_70.py
@@ -132,16 +132,18 @@ def dropIndexLike(apps, schema_editor):
             )
             select cte.index_name
             from information_schema.table_constraints tco
-            join information_schema.key_column_usage kcu
-            on tco.constraint_schema = kcu.constraint_schema
-            and tco.constraint_name = kcu.constraint_name
-            join information_schema.referential_constraints rco
-            on tco.constraint_schema = rco.constraint_schema
-            and tco.constraint_name = rco.constraint_name
-            join information_schema.table_constraints rel_tco
-            on rco.unique_constraint_schema = rel_tco.constraint_schema
-            and rco.unique_constraint_name = rel_tco.constraint_name
-            inner join cte on cte.table_name = kcu.table_name and cte.column_names = kcu.column_name
+            inner join information_schema.key_column_usage kcu
+              on tco.constraint_schema = kcu.constraint_schema
+              and tco.constraint_name = kcu.constraint_name
+            inner join cte
+              on cte.table_name = kcu.table_name
+              and cte.column_names = kcu.column_name
+            inner join information_schema.referential_constraints rco
+              on tco.constraint_schema = rco.constraint_schema
+              and tco.constraint_name = rco.constraint_name
+            inner join information_schema.table_constraints rel_tco
+              on tco.constraint_schema = rel_tco.constraint_schema
+              and tco.constraint_name = rel_tco.constraint_name
             where tco.constraint_type = 'FOREIGN KEY'
             and kcu.table_name in ('buffer', 'calendarbucket', 'customer', 'demand', 'item',
                                 'itemdistribution', 'itemsupplier', 'location', 'operation',

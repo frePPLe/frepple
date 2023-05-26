ebec4bce6f61fb98df14c73dca4cf3f0b5e7d1bb
Johan De Taeye <jdetaeye@frepple.com>
2023-05-26 15:03:51 +0200

    granting report role read access to archive tables

---
+++ freppledb/archive/migrations/0005_grant_read.py
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2023 by frePPLe bv
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from django.db import migrations, connections
+from django.conf import settings
+
+
+def grant_read_access(apps, schema_editor):
+    db = schema_editor.connection.alias
+    role = settings.DATABASES[db].get("SQL_ROLE", "report_role")
+    if role:
+        with connections[db].cursor() as cursor:
+            cursor.execute("select count(*) from pg_roles where rolname = %s", (role,))
+            if not cursor.fetchone()[0]:
+                cursor.execute(
+                    "create role %s with nologin noinherit role current_user" % (role,)
+                )
+            for table in ["ax_demand", "ax_buffer", "ax_operationplan"]:
+                cursor.execute("grant select on table %s to %s" % (table, role))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("archive", "0004_truncate_archive"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=grant_read_access,
+        ),
+    ]

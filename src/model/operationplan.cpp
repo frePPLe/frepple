ef254e1fa338c0ec45b795c65388420d3b8bc1a3
Johan De Taeye <jdetaeye@frepple.com>
2024-02-16 16:24:54 +0100

    revert a commit.   Date objects are intialized automatically to 1/1/1971.  No need to explicitly set to 0.

--- src/model/operationplan.cpp
+++ src/model/operationplan.cpp
@@ -2750,7 +2750,7 @@ PyObject* OperationPlan::getColorPython(PyObject* self, PyObject* args) {
 
   // Remaining possibilities now, POs, DOs and regular timer_per, fixed_time
   // MOs, no subops
-  Date firstProposedStart = nullptr;
+  Date firstProposedStart;
   for (auto rr = opplan->getOperation()->getOperationPlans();
        rr != OperationPlan::end(); ++rr) {
     if (!(&*rr)->getProposed()) continue;

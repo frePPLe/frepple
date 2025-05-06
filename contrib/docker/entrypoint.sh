66bff5932f52403458e75b620ce5cc78065571f9
hichamlahlou <hlahlou@frepple.com>
2025-05-06 15:40:06 +0200

    populate initial data

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -31,6 +31,27 @@ done
 frepplectl createdatabase --skip-if-exists
 frepplectl migrate --noinput
 
+# populate the database with initial data
+should_run_populate=false
+
+# Loop through all command line arguments to see if populate is set
+for arg in "$@"; do
+    if [ "$arg" = "populate" ]; then
+        should_run_populate=true
+        break
+    fi
+done
+
+if [ "$should_run_populate" = true ]; then
+    echo "Populating initial data"
+    frepplectl scenario_copy default scenario1 --description="distribution demo"
+    frepplectl scenario_copy default scenario2 --description="manufacturing demo"
+    frepplectl loaddata --database=scenario1 distribution_demo --verbosity=0
+    frepplectl runplan --database=scenario1 --env=fcst,supply --background
+    frepplectl loaddata --database=scenario2 manufacturing_demo --verbosity=0
+    frepplectl runplan --database=scenario2 --env=fcst,supply --background
+fi
+
 # Configure Apache
 grep -qxF 'ServerName' /etc/apache2/apache2.conf || echo "ServerName localhost" >> /etc/apache2/apache2.conf
 rm -f /usr/local/apache2/logs/httpd.pid

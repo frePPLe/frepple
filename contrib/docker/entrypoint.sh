a962c4a15807ec7536e40e52b98d5354a3e3fe24
hichamlahlou <hlahlou@frepple.com>
2025-05-08 13:10:20 +0200

    make the file unique for all deployments

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -51,9 +51,9 @@ else
     frepplectl scenario_copy default scenario1 --description="distribution demo"
     frepplectl scenario_copy default scenario2 --description="manufacturing demo"
     frepplectl loaddata --database=scenario1 distribution_demo --verbosity=0
-    frepplectl runplan --database=scenario1 --env=fcst,supply --background
+    frepplectl runplan --database=scenario1 --env=fcst,invplan,balancing,supply --background
     frepplectl loaddata --database=scenario2 manufacturing_demo --verbosity=0
-    frepplectl runplan --database=scenario2 --env=fcst,supply --background
+    frepplectl runplan --database=scenario2 --env=fcst,invplan,balancing,supply --background
 fi
 
 # Configure Apache

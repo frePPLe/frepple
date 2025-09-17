71dd900ff81734915d7d049c001e3c7532160fcf
Johan De Taeye <jdetaeye@frepple.com>
2025-09-17 15:33:55 +0200

    recreate the db-level cascaded-deletes

--- freppledb/archive/migrations/0007_varchar_max_length.py
+++ freppledb/archive/migrations/0007_varchar_max_length.py
@@ -21,7 +21,65 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-from django.db import migrations, models
+from django.db import migrations, models, connections
+
+
+def recreateConstraintsCascade(apps, schema_editor):
+    def getConstraint(cursor, tableName):
+        cursor.execute(
+            """
+            select conname from pg_constraint
+            inner join pg_class opplan on opplan.oid = pg_constraint.confrelid and opplan.relname = 'ax_manager'
+            inner join pg_class opm on opm.oid = pg_constraint.conrelid and opm.relname = %s
+            inner join pg_attribute on attname = 'snapshot_date_id' and attrelid = opm.oid and pg_attribute.attnum = any(conkey)
+            """,
+            (tableName,),
+        )
+        return cursor.fetchone()[0]
+
+    with connections[schema_editor.connection.alias].cursor() as cursor:
+        cursor.execute(
+            "alter table ax_demand drop constraint %s"
+            % (getConstraint(cursor, "ax_demand"),)
+        )
+        cursor.execute(
+            "alter table ax_buffer drop constraint %s"
+            % (getConstraint(cursor, "ax_buffer"),)
+        )
+        cursor.execute(
+            "alter table ax_operationplan drop constraint %s"
+            % (getConstraint(cursor, "ax_operationplan"),)
+        )
+        cursor.execute(
+            """
+            alter table ax_demand
+            ADD FOREIGN KEY (snapshot_date_id)
+            REFERENCES public.ax_manager (snapshot_date) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            DEFERRABLE INITIALLY DEFERRED
+            """
+        )
+        cursor.execute(
+            """
+            alter table ax_buffer
+            ADD FOREIGN KEY (snapshot_date_id)
+            REFERENCES public.ax_manager (snapshot_date) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            DEFERRABLE INITIALLY DEFERRED
+            """
+        )
+        cursor.execute(
+            """
+            alter table ax_operationplan
+            ADD FOREIGN KEY (snapshot_date_id)
+            REFERENCES public.ax_manager (snapshot_date) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            DEFERRABLE INITIALLY DEFERRED
+            """
+        )
 
 
 class Migration(migrations.Migration):
@@ -150,4 +208,5 @@ class Migration(migrations.Migration):
             name="type",
             field=models.CharField(db_index=True, verbose_name="type"),
         ),
+        migrations.RunPython(recreateConstraintsCascade, migrations.RunPython.noop),
     ]
--- freppledb/input/migrations/0079_varchar_max_length.py
+++ freppledb/input/migrations/0079_varchar_max_length.py
@@ -23,7 +23,51 @@
 
 import datetime
 
-from django.db import migrations, models
+from django.db import migrations, models, connections
+
+
+def recreateConstraintsCascade(apps, schema_editor):
+    def getConstraint(cursor, tableName):
+        cursor.execute(
+            """
+            select conname from pg_constraint
+            inner join pg_class opplan on opplan.oid = pg_constraint.confrelid and opplan.relname = 'operationplan'
+            inner join pg_class opm on opm.oid = pg_constraint.conrelid and opm.relname = %s
+            inner join pg_attribute on attname = 'operationplan_id' and attrelid = opm.oid and pg_attribute.attnum = any(conkey)
+            """,
+            (tableName,),
+        )
+        return cursor.fetchone()[0]
+
+    with connections[schema_editor.connection.alias].cursor() as cursor:
+        cursor.execute(
+            "alter table operationplanmaterial drop constraint %s"
+            % (getConstraint(cursor, "operationplanmaterial"),)
+        )
+        cursor.execute(
+            "alter table operationplanresource drop constraint %s"
+            % (getConstraint(cursor, "operationplanresource"),)
+        )
+        cursor.execute(
+            """
+            alter table operationplanresource
+            ADD FOREIGN KEY (operationplan_id)
+            REFERENCES public.operationplan (reference) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            DEFERRABLE INITIALLY DEFERRED
+            """
+        )
+        cursor.execute(
+            """
+            alter table operationplanmaterial
+                ADD FOREIGN KEY (operationplan_id)
+                REFERENCES public.operationplan (reference) MATCH SIMPLE
+                ON UPDATE NO ACTION
+                ON DELETE CASCADE
+                DEFERRABLE INITIALLY DEFERRED
+            """
+        )
 
 
 class Migration(migrations.Migration):
@@ -1288,4 +1332,5 @@ class Migration(migrations.Migration):
                 blank=True, db_index=True, null=True, verbose_name="subcategory"
             ),
         ),
+        migrations.RunPython(recreateConstraintsCascade, migrations.RunPython.noop),
     ]

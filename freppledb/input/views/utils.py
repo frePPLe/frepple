2c5de1458ddbbe449e22cc56ead5f865e736a88b
Johan De Taeye <jdetaeye@frepple.com>
2025-11-25 16:15:33 +0100

    displaying info of opplan moves in operationplan detail screens

--- freppledb/input/views/utils.py
+++ freppledb/input/views/utils.py
@@ -2176,6 +2176,8 @@ class OperationPlanDetail(View):
                     }
                 if hasattr(opplan, "remark"):
                     res["remark"] = getattr(opplan, "remark", None)
+                if "info" in getattr(opplan, "plan", {}):
+                    res["info"] = opplan.plan["info"]
 
                 # Information on materials
                 if view_OpplanMaterial:

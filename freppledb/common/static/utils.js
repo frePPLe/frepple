7f93c39c14e684a6f2a2f794fd61a61aa2eb7662
Johan De Taeye <jdetaeye@frepple.com>
2026-01-23 12:50:23 +0100

    fix license headers + vue cleanup

--- freppledb/common/static/utils.js
+++ freppledb/common/static/utils.js
@@ -1,66 +1,77 @@
-import { ref as c } from "vue";
-const p = (e) => e == null, g = (e) => e && typeof e == "object", b = (e) => p(e) || Array.isArray(e) && e.length === 0 || g(e) && Object.keys(e).length === 0 || typeof e == "string" && e.trim().length === 0, F = (e) => e === "" || e === null || e === void 0 ? !1 : !isNaN(parseFloat(e)) && isFinite(e), w = () => {
-  const e = c(window.database);
+import { ref as d } from "vue";
+const p = (e) => e == null, a = (e) => e && typeof e == "object", b = (e) => p(e) || Array.isArray(e) && e.length === 0 || a(e) && Object.keys(e).length === 0 || typeof e == "string" && e.trim().length === 0, g = (e) => e === "" || e === null || e === void 0 ? !1 : !isNaN(parseFloat(e)) && isFinite(e), w = () => {
+  const e = d(window.database);
   return e.value === "default" ? "" : `/${e.value}`;
-}, S = (e, r) => {
+}, y = (e, r) => {
   if (!e) return "";
-  const n = new Date(e);
-  return moment(n).format(window.datetimeformat);
+  const o = new Date(e);
+  return moment(o).format(window.datetimeformat);
 };
-function y(e, r = { reactive: !1 }) {
-  return c(window[e]);
+function S(e, r = { reactive: !1 }) {
+  return d(window[e]);
 }
 const h = (e, r) => {
   if (!e) return "";
-  const n = new Date(e);
-  return moment(n).format(window.dateformat);
+  const o = new Date(e);
+  return moment(o).format(window.dateformat);
 }, j = (e) => {
   if (!e) return "";
   const r = new Date(e);
   return moment(r).format("HH:mm:ss");
 };
 function N(e, r = 300) {
-  let n;
-  return function(...o) {
+  let o;
+  return function(...n) {
     const t = () => {
-      n = null, e.apply(this, o);
+      o = null, e.apply(this, n);
     };
-    clearTimeout(n), n = setTimeout(t, r), n || e.apply(this, o);
+    clearTimeout(o), o = setTimeout(t, r), o || e.apply(this, n);
   };
 }
-const x = (e, r = 6) => {
+const T = (e, r = 6) => {
   if (typeof e > "u" || e === "")
     return "";
-  if (F(e)) {
+  if (g(e)) {
     e *= 1;
-    const l = e < 0, o = Math.abs(e);
+    const u = e < 0, n = Math.abs(e);
     let t = 0;
-    o > 1e5 || r <= 0 ? t = String(parseFloat(e.toFixed())) : o > 1e4 || r <= 1 ? t = String(parseFloat(e.toFixed(1))) : o > 1e3 || r <= 2 ? t = String(parseFloat(e.toFixed(2))) : o > 100 || r <= 3 ? t = String(parseFloat(e.toFixed(3))) : o > 10 || r <= 4 ? t = String(parseFloat(e.toFixed(4))) : o > 1 || r <= 5 ? t = String(parseFloat(e.toFixed(5))) : t = String(parseFloat(e.toFixed(r))), t = (l ? "-" : "") + t;
-    const f = jQuery("#grid").jqGrid("getGridRes", "formatter.number.decimalSeparator") || ".";
-    f !== "." && (t = t.replace(".", f));
+    n > 1e5 || r <= 0 ? t = String(parseFloat(e.toFixed())) : n > 1e4 || r <= 1 ? t = String(parseFloat(e.toFixed(1))) : n > 1e3 || r <= 2 ? t = String(parseFloat(e.toFixed(2))) : n > 100 || r <= 3 ? t = String(parseFloat(e.toFixed(3))) : n > 10 || r <= 4 ? t = String(parseFloat(e.toFixed(4))) : n > 1 || r <= 5 ? t = String(parseFloat(e.toFixed(5))) : t = String(parseFloat(e.toFixed(r))), t = (u ? "-" : "") + t;
+    const l = jQuery("#grid").jqGrid("getGridRes", "formatter.number.decimalSeparator") || ".";
+    l !== "." && (t = t.replace(".", l));
     const m = jQuery("#grid").jqGrid("getGridRes", "formatter.number.thousandsSeparator") || ",";
-    if (o >= 1e3) {
-      let i = t.lastIndexOf(f);
+    if (n >= 1e3) {
+      let i = t.lastIndexOf(l);
       i = i > -1 ? i : t.length;
-      let u = f === void 0 ? "" : t.substring(i), d = -1, s;
+      let f = l === void 0 ? "" : t.substring(i), c = -1, s;
       for (s = i; s > 0; s--)
-        d++, d % 3 === 0 && s !== i && (!l || s > 1) && (u = m + u), u = t.charAt(s - 1) + u;
-      t = u;
+        c++, c % 3 === 0 && s !== i && (!u || s > 1) && (f = m + f), f = t.charAt(s - 1) + f;
+      t = f;
     }
     return t.replace("--", "-");
   }
   return (e == null ? void 0 : e.toLocaleString()) || "0";
 };
+function x(e, r = 150) {
+  let o;
+  return (...u) => {
+    clearTimeout(o), o = setTimeout(() => e(...u), r);
+  };
+}
+function I(e) {
+  return typeof window.admin_escape == "function" ? window.admin_escape(e) : encodeURIComponent(e);
+}
 export {
+  I as adminEscape,
   h as dateFormat,
-  S as dateTimeFormat,
+  y as dateTimeFormat,
+  x as debounce,
   N as debouncedInputHandler,
-  y as getDjangoTemplateVariable,
+  S as getDjangoTemplateVariable,
   w as getURLprefix,
   b as isBlank,
   p as isEmpty,
-  F as isNumeric,
-  g as isObject,
-  x as numberFormat,
+  g as isNumeric,
+  a as isObject,
+  T as numberFormat,
   j as timeFormat
 };
